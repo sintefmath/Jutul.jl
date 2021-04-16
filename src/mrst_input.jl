632065565e472f9ddb797572398726b482e7ea07
Olav Møyner <olav.moyner@gmail.com>
2021-04-16 16:23:54 +0200

    Add minimal grid builder for MRST input

--- src/mrst_input.jl
+++ src/mrst_input.jl
@@ -1,6 +1,7 @@
 using MAT # .MAT file loading
 export readSimGraph, MRSTSimGraph, getSparsity, getIncompMatrix
 export readPatchPlot
+export get_minimal_tpfa_grid_from_mrst
 using SparseArrays # Sparse pattern
 
 struct MRSTSimGraph
@@ -24,10 +25,63 @@ struct MRSTPlotData
 end
 
 
-function get_minimal_grid(mat_path::String)
-    exported = MAT.matread(string("data/testgrids/", filename, ".mat"))
-    normals = exported["G"]["faces"]["normals"]./exported["G"]["faces"]["areas"];
+function get_minimal_tpfa_grid_from_mrst(name::String; relative_path=true)
+    if relative_path
+        fn = string("data/testgrids/", name, ".mat")
+    else
+        fn = name
+    end
+    exported = MAT.matread(fn)
+    N = exported["G"]["faces"]["neighbors"]
+    N = Int64.(N)
+    
+    internal_faces = (N[:, 2] .> 0) .& (N[:, 1] .> 0)
+    N = copy(N[internal_faces, :]')
+    
+    # get_cell_faces(N)
+    # get_cell_neighbors(N)
+    
+    # Cells
+    cell_centroids = copy((exported["G"]["cells"]["centroids"])')
+    # Faces
+    face_centroids = copy((exported["G"]["faces"]["centroids"][internal_faces, :])')
+    face_areas = vec(exported["G"]["faces"]["areas"][internal_faces])
+    face_normals = exported["G"]["faces"]["normals"][internal_faces, :]./face_areas
+    face_normals = copy(face_normals')
+    
+    perm = copy((exported["rock"]["perm"])')
 
+    # Deal with cell data
+    poro = vec(exported["rock"]["poro"])
+    volumes = vec(exported["G"]["cells"]["volumes"])
+    pv = poro.*volumes
+    nc = length(pv)
+
+    # Deal with face data
+    T_hf = compute_half_face_trans(cell_centroids, face_centroids, face_normals, face_areas, perm, N)
+    T = compute_face_trans(T_hf, N)
+
+    nhf = length(T_hf)
+    faces, facePos = get_facepos(N)
+
+    float_type = typeof(T[1])
+    index_type = typeof(N[1])
+    t = TPFAHalfFaceData{float_type, index_type}
+    faceData = Vector{t}(undef, nhf)
+
+    for cell = 1:nc
+        for fpos = facePos[cell]:(facePos[cell+1]-1)
+            face = faces[fpos]
+            if N[1, face] == cell
+                other = N[2, face]
+            else
+                other = N[1, face]
+            end
+            dz = cell_centroids[3, cell] - cell_centroids[3, other]
+            faceData[fpos] = t(T[face], dz, cell, other)
+        end
+    end
+    sg = MinimalTPFAGrid(faceData, pv)
 end
 
 
