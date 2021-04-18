93f844450ad55c09db4019d33c832b55050b5931
Olav Møyner <olav.moyner@gmail.com>
2021-04-18 16:01:37 +0200

    Update equation_helpers.jl
    add allocator

--- src/equation_helpers.jl
+++ src/equation_helpers.jl
@@ -1,4 +1,4 @@
-export ConservationLaw, setup_conservationlaw
+export ConservationLaw, setup_conservationlaw, allocate_vector_ad
 
 struct ConservationLaw
     accumulation
@@ -8,19 +8,23 @@ end
 function setup_conservationlaw(G::TervGrid, nder = 0, hasAcc = true; T=Float64)
     nc = number_of_cells(G)
     nf = number_of_half_faces(G)
-    acc = zeros(T, nc)
-    if nder == 0
-        dt = T
-    else
-        d = ForwardDiff.Dual(zeros(nder+1)...)
-        dt = typeof(d)
-    end
     if hasAcc
-        acc = Vector{dt}(undef, nc)
+        acc = allocate_vector_ad(nc, nder, T=T)
     else
         acc = nothing
     end
-    flux = Vector{dt}(undef, 2*nf)
+    flux = allocate_vector_ad(2*nf, nder, T=T)
     ConservationLaw(acc, flux)
     # ConservationLaw{T}(acc, flux)
+end
+
+function allocate_vector_ad(n::R, nder = 0; T=Float64) where {R<:Integer}
+    # allocate a n length vector with space for derivatives
+    if nder == 0
+        return Vector{T}(undef, n)
+    else
+        d = ForwardDiff.Dual{T}(zeros(nder+1)...)
+        return Vector{typeof(d)}(undef, n)
+    end
+    
 end
