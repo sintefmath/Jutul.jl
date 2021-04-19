1a9734a71814504464a1ff1df536f7a709f76291
Olav Møyner <olav.moyner@gmail.com>
2021-04-19 16:08:15 +0200

    Update equation_helpers.jl

--- src/equation_helpers.jl
+++ src/equation_helpers.jl
@@ -1,4 +1,6 @@
-export ConservationLaw, setup_conservationlaw, allocate_vector_ad, get_ad_unit_scalar
+export ConservationLaw, setup_conservationlaw
+export allocate_vector_ad, get_ad_unit_scalar, update_values!
+export allocate_residual, allocate_jacobian
 
 struct ConservationLaw
     accumulation
@@ -6,6 +8,7 @@ struct ConservationLaw
 end
 
 function setup_conservationlaw(G::TervGrid, nder = 0, hasAcc = true; T=Float64)
+    # Should be made into a normal constructor when I figure out the syntax
     nc = number_of_cells(G)
     nf = number_of_half_faces(G)
     if hasAcc
@@ -18,25 +21,34 @@ function setup_conservationlaw(G::TervGrid, nder = 0, hasAcc = true; T=Float64)
     # ConservationLaw{T}(acc, flux)
 end
 
+function allocate_residual(G::TervGrid, Law::ConservationLaw)
+    r = similar(Law.accumulation, number_of_cells(G))
+    return r
+end
+
+function allocate_jacobian(G::TervGrid, Law::ConservationLaw)
+    jac = get_incomp_matrix(G) # Just a hack for the time being
+    return jac
+end
+
 function allocate_vector_ad(n::R, nder = 0; T = Float64, diag_pos = nothing) where {R<:Integer}
-    # allocate a n length vector with space for derivatives
+    # allocate a n length zero vector with space for derivatives
     if nder == 0
-        return Vector{T}(undef, n)
+        return Vector{T}(0, n)
     else
-        # d = ForwardDiff.Dual{T}(zeros(nder+1)...)
         d = get_ad_unit_scalar(T(0.0), nder, diag_pos)
         return Vector{typeof(d)}(undef, n)
     end
 end
 
-function get_ad_unit_scalar(v::T, nder, diag_pos = nothing, diag_val = 1) where {T<:Real}
+function get_ad_unit_scalar(v::T, nder, diag_pos = nothing) where {T<:Real}
     # Get a scalar, with a given number of zero derivatives. A single entry can be specified to be non-zero
     if nder > 0
-        if isnothing(diag_pos)
-            v = ForwardDiff.Dual{T}([v, zeros(nder+1)...]...)
-        else
-            v = ForwardDiff.Dual{T}([v, zeros(diag_pos-1)..., diag_val, zeros(nder-diag_pos)...]...)
-        end
+        v = ForwardDiff.Dual{T}(v, ntuple(x -> T(x == diag_pos), nder))
     end
     return v
+end
+
+function update_values!(v::AbstractArray, next::AbstractArray)
+    v .= v - value(v) + next
 end
