1a782c5ad944684820ea3cab816af121ebd37b93
Olav Møyner <olav.moyner@gmail.com>
2021-05-15 13:54:25 +0200

    multimodel test: Verify both models first

--- dev/simple_multimodel_test.jl
+++ dev/simple_multimodel_test.jl
@@ -1,6 +1,6 @@
 using Terv
 ENV["JULIA_DEBUG"] = Terv
-
+##
 sys = ScalarTestSystem()
 A = ScalarTestDomain()
 B = ScalarTestDomain()
@@ -11,10 +11,15 @@ modelB = SimulationModel(B, sys)
 sourceA = ScalarTestForce(1.0)
 forcesA = build_forces(modelA, sources = sourceA)
 state0A = setup_state(modelA, Dict("XVar"=>1.0))
-sim = Simulator(modelA, state0 = state0A)
-states = simulate(sim, [1.0], forces = forcesA)
+simA = Simulator(modelA, state0 = state0A)
+statesA = simulate(simA, [1.0], forces = forcesA)
 
-
+## Test the second model
+sourceB = ScalarTestForce(-1.0)
+forcesB = build_forces(modelB, sources = sourceB)
+state0B = setup_state(modelB, Dict("XVar"=>1.0))
+simB = Simulator(modelB, state0 = state0B)
+statesB = simulate(simB, [1.0], forces = forcesB)
 
 ## Make a multimodel
 model = MultiModel((A = modelA, B = modelB))
