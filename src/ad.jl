7cd77dfd77df1df9f38b1244f284fed5fda4dadc
Olav Møyner <olav.moyner@gmail.com>
2021-05-25 12:54:59 +0200

    Generalize alignment function

--- src/ad.jl
+++ src/ad.jl
@@ -77,14 +77,26 @@ function update_linearized_system_subset!(jac, r, model, cache::TervAutoDiffCach
     end
 end
 
-function diagonal_alignment!(cache::TervAutoDiffCache, jac, layout; eq_index = 1:cache.number_of_units, target_offset = 0, source_offset = 0)
+function diagonal_alignment!(cache, arg...; eq_index = 1:cache.number_of_units, kwarg...)
+    injective_alignment!(cache, arg...; target_index = eq_index, source_index = eq_index, kwarg...)
+end
+
+function injective_alignment!(cache::TervAutoDiffCache, jac, layout;
+                                    target_index = 1:cache.number_of_units,
+                                    source_index = 1:cache.number_of_units,
+                                    target_offset = 0,
+                                    source_offset = 0)
     nu, ne, np = ad_dims(cache)
-    nix = length(eq_index)
-    for i in eq_index
+    nix = length(target_index)
+    N = length(source_index)
+    @assert length(target_index) == N
+    for index in 1:N
+        target = target_index[index]
+        source = source_index[index]
         for e in 1:ne
             for d = 1:np
-                pos = find_jac_position(jac, i + target_offset, i + source_offset, e, d, nix, nu, ne, np, layout)
-                set_jacobian_pos!(cache, i, e, d, pos)
+                pos = find_jac_position(jac, target + target_offset, source + source_offset, e, d, nix, nu, ne, np, layout)
+                set_jacobian_pos!(cache, target, e, d, pos)
             end
         end
     end
