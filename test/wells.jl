d1dc00cea246382254c660c115b966c2088ba9f4
Olav Møyner <olav.moyner@gmail.com>
2021-06-11 13:55:40 +0200

    Better initial guess.

--- test/wells.jl
+++ test/wells.jl
@@ -59,7 +59,7 @@ function build_well(mrst_data, ix)
 end
 
 # Initial condition for all wells
-w0 = Dict(:Pressure => p0, :TotalMassFlux => 1.0)
+w0 = Dict(:Pressure => p0, :TotalMassFlux => 1e-12)
 
 # Rate injector
 Wi = build_well(mrst_data, 1)
