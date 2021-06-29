8c10c9f53f6f5337ffd50b868060c1d39e76f4da
Olav Møyner <olav.moyner@gmail.com>
2021-06-29 08:59:20 +0200

    Clean up Egg example, fix SWOF table

--- test/scripts/simple_egg.jl
+++ test/scripts/simple_egg.jl
@@ -14,160 +14,166 @@ casename = "egg"
 # casename = "mini"
 simple_well = false
 
+function run_immiscible_mrst(casename, simple_well = false)
+    G, mrst_data = get_minimal_tpfa_grid_from_mrst(casename, extraout = true, fuse_flux = false)
+    ## Set up initializers
+    models = OrderedDict()
+    initializer = Dict()
+    forces = Dict()
+    ## Set up reservoir part
+    f = mrst_data["fluid"]
+    p = vec(f["p"])
+    c = vec(f["c"])
+    mu = vec(f["mu"])
+    nkr = vec(f["nkr"])
+    rhoS = vec(f["rhoS"])
 
-G, mrst_data = get_minimal_tpfa_grid_from_mrst(casename, extraout = true, fuse_flux = false)
-## Set up initializers
-models = OrderedDict()
-initializer = Dict()
-forces = Dict()
-## Set up reservoir part
-f = mrst_data["fluid"]
-p = vec(f["p"])
-c = vec(f["c"])
-mu = vec(f["mu"])
-nkr = vec(f["nkr"])
-rhoS = vec(f["rhoS"])
 
+    water = AqueousPhase()
+    oil = LiquidPhase()
+    sys = ImmiscibleSystem([water, oil])
+    model = SimulationModel(G, sys)
 
-water = AqueousPhase()
-oil = LiquidPhase()
-sys = ImmiscibleSystem([water, oil])
-model = SimulationModel(G, sys)
+    rho = ConstantCompressibilityDensities(sys, p, rhoS, c)
 
-rho = ConstantCompressibilityDensities(sys, p, rhoS, c)
+    swof = f["swof"]
 
-swof = f["swof"]
-
-if isempty(swof)
-    kr = BrooksCoreyRelPerm(sys, nkr)
-else
-    sw = swof[:, 1]
-    so = 1 .- sw
-    s = [sw, so]
-    krt = vcat(swof[:, 2]', swof[end:-1:1, 3]')
-    kr = TabulatedRelPermSimple(s, krt)
-end
-mu = ConstantVariables(mu)
-
-s = model.secondary_variables
-s[:PhaseMassDensities] = rho
-s[:RelativePermeabilities] = kr
-s[:PhaseViscosities] = mu
-
-##
-state0 = mrst_data["state0"]
-p0 = state0["pressure"]
-if isa(p0, AbstractArray)
-    p0 = vec(p0)
-else
-    p0 = [p0]
-end
-s0 = state0["s"]'
-init = Dict(:Pressure => p0, :Saturations => s0)
-state0r = setup_state(model, init)
-
-## Model parameters
-param_res = setup_parameters(model)
-param_res[:reference_densities] = vec(rhoS)
-
-dt = mrst_data["dt"]
-if isa(dt, Real)
-    dt = [dt]
-end
-timesteps = vec(dt)
-sim = Simulator(model, state0 = state0r, parameters = param_res)
-
-# simulate(sim, timesteps)
-
-initializer[:Reservoir] = init
-forces[:Reservoir] = nothing
-models[:Reservoir] = model
-
-## Set up injector
-
-# Initial condition for all wells
-slw = 1.0
-slw = 0.0
-w0 = Dict(:Pressure => mean(p0), :TotalMassFlux => 1e-12, :Saturations => [slw, 1-slw])
-
-well_symbols = map((x) -> Symbol(x["name"]), vec(mrst_data["W"]))
-num_wells = length(well_symbols)
-
-well_parameters = Dict()
-controls = Dict()
-for i = 1:num_wells
-    sym = well_symbols[i]
-
-    wi, wdata = get_well_from_mrst_data(mrst_data, sys, i, extraout = true, volume = 1e-3, simple = simple_well)
-
-    sv = wi.secondary_variables
-    sv[:PhaseMassDensities] = rho
-    sv[:PhaseViscosities] = mu
-
-    models[sym] = wi
-
-    println("$sym")
-    t_mrst = wdata["val"]
-    is_injector = wdata["sign"] > 0
-    is_shut = wdata["status"] < 1
-    if wdata["type"] == "rate"
-        println("Rate")
-        target = SinglePhaseRateTarget(t_mrst, AqueousPhase())
+    if isempty(swof)# || true
+        kr = BrooksCoreyRelPerm(sys, nkr)
     else
-        println("BHP")
-        target = BottomHolePressureTarget(t_mrst)
+        sw = swof[:, 1]
+        krw = swof[:, 2]'
+        # Change so table to be with respect to so,
+        # and to be increasing with respect to input
+        so = 1 .- sw
+        so = so[end:-1:1]
+        kro = swof[end:-1:1, 3]'
+        s = [sw, so]
+        krt = vcat(krw, kro)
+        kr = TabulatedRelPermSimple(s, krt)
     end
 
-    if is_shut
-        println("Shut well")
-        ctrl = DisabledControl()
-    elseif is_injector
-        println("Injector")
-        ctrl = InjectorControl(target, wdata["compi"])
+    mu = ConstantVariables(mu)
+
+    s = model.secondary_variables
+    s[:PhaseMassDensities] = rho
+    s[:RelativePermeabilities] = kr
+    s[:PhaseViscosities] = mu
+
+    ##
+    state0 = mrst_data["state0"]
+    p0 = state0["pressure"]
+    if isa(p0, AbstractArray)
+        p0 = vec(p0)
     else
-        println("Producer")
-        ctrl = ProducerControl(target)
+        p0 = [p0]
     end
-    param_w = setup_parameters(wi)
-    param_w[:tolerances][:mass_conservation] = 1e-1
-    param_w[:reference_densities] = vec(rhoS)
+    s0 = state0["s"]'
+    init = Dict(:Pressure => p0, :Saturations => s0)
+    state0r = setup_state(model, init)
 
-    well_parameters[sym] = param_w
-    controls[sym] = ctrl
-    forces[sym] = nothing
-    initializer[sym] = w0
+    ## Model parameters
+    param_res = setup_parameters(model)
+    param_res[:reference_densities] = vec(rhoS)
+
+    dt = mrst_data["dt"]
+    if isa(dt, Real)
+        dt = [dt]
+    end
+    timesteps = vec(dt)
+    sim = Simulator(model, state0 = state0r, parameters = param_res)
+
+    # simulate(sim, timesteps)
+
+    initializer[:Reservoir] = init
+    forces[:Reservoir] = nothing
+    models[:Reservoir] = model
+
+    ## Set up injector
+
+    # Initial condition for all wells
+    slw = 1.0
+    slw = 0.0
+    w0 = Dict(:Pressure => mean(p0), :TotalMassFlux => 1e-12, :Saturations => [slw, 1-slw])
+
+    well_symbols = map((x) -> Symbol(x["name"]), vec(mrst_data["W"]))
+    num_wells = length(well_symbols)
+
+    well_parameters = Dict()
+    controls = Dict()
+    for i = 1:num_wells
+        sym = well_symbols[i]
+
+        wi, wdata = get_well_from_mrst_data(mrst_data, sys, i, extraout = true, volume = 1e-3, simple = simple_well)
+
+        sv = wi.secondary_variables
+        sv[:PhaseMassDensities] = rho
+        sv[:PhaseViscosities] = mu
+
+        models[sym] = wi
+
+        println("$sym")
+        t_mrst = wdata["val"]
+        is_injector = wdata["sign"] > 0
+        is_shut = wdata["status"] < 1
+        if wdata["type"] == "rate"
+            println("Rate")
+            target = SinglePhaseRateTarget(t_mrst, AqueousPhase())
+        else
+            println("BHP")
+            target = BottomHolePressureTarget(t_mrst)
+        end
+
+        if is_shut
+            println("Shut well")
+            ctrl = DisabledControl()
+        elseif is_injector
+            println("Injector")
+            ctrl = InjectorControl(target, wdata["compi"])
+        else
+            println("Producer")
+            ctrl = ProducerControl(target)
+        end
+        param_w = setup_parameters(wi)
+        param_w[:tolerances][:mass_conservation] = 1e-1
+        param_w[:reference_densities] = vec(rhoS)
+
+        well_parameters[sym] = param_w
+        controls[sym] = ctrl
+        forces[sym] = nothing
+        initializer[sym] = w0
+    end
+    ##
+    g = WellGroup(well_symbols)
+    mode = PredictionMode()
+    WG = SimulationModel(g, mode)
+    F0 = Dict(:TotalSurfaceMassRate => 0.0)
+
+    facility_forces = build_forces(WG, control = controls)
+    models[:Facility] = WG
+    initializer[:Facility] = F0
+    ##
+
+    mmodel = MultiModel(convert_to_immutable_storage(models))
+    # Set up joint state and simulate
+    state0 = setup_state(mmodel, initializer)
+    forces[:Facility] = facility_forces
+
+    parameters = setup_parameters(mmodel)
+    parameters[:Reservoir] = param_res
+
+    for w in well_symbols
+        parameters[w] = well_parameters[w]
+    end
+
+    sim = Simulator(mmodel, state0 = state0, parameters = deepcopy(parameters))
+    # dt = [1.0]
+    # dt = [1.0, 1.0, 10.0, 10.0, 100.0]*3600*24
+    dt = timesteps
+    simulate(sim, dt, forces = forces, info_level = 1)
 end
 ##
-g = WellGroup(well_symbols)
-mode = PredictionMode()
-WG = SimulationModel(g, mode)
-F0 = Dict(:TotalSurfaceMassRate => 0.0)
-fstate = setup_state(WG, F0)
-
-facility_forces = build_forces(WG, control = controls)
-models[:Facility] = WG
-initializer[:Facility] = F0
-##
-
-mmodel = MultiModel(convert_to_immutable_storage(models))
-# mmodel = MultiModel((Reservoir = model, Injector = Wi, Producer = Wp, Facility = WG))
-# Set up joint state and simulate
-state0 = setup_state(mmodel, initializer)
-forces[:Facility] = facility_forces
-# forces = Dict(:Reservoir => nothing, :Facility => facility_forces, :Injector => nothing, :Producer => nothing)
-
-parameters = setup_parameters(mmodel)
-parameters[:Reservoir] = param_res
-
-for w in well_symbols
-    parameters[w] = well_parameters[w]
-end
-
-sim = Simulator(mmodel, state0 = state0, parameters = deepcopy(parameters))
-# dt = [1.0]
-# dt = [1.0, 1.0, 10.0, 10.0, 100.0]*3600*24
-dt = timesteps
-states = simulate(sim, dt, forces = forces, info_level = 1)
+states = run_immiscible_mrst(casename, false)
 nothing
 ##
 d = map((x) -> x[:Reservoir][:Pressure][1], states)
