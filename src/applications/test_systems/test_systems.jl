b044ccabee00af7623a2f07941e84b9e3c645508
Olav Møyner <olav.moyner@gmail.com>
2021-07-09 14:21:01 +0200

    Update test_systems.jl

--- src/applications/test_systems/test_systems.jl
+++ src/applications/test_systems/test_systems.jl
@@ -9,7 +9,7 @@ function number_of_cells(::ScalarTestDomain) 1 end
 
 function get_domain_intersection(u::TervUnit, target_d::ScalarTestDomain, source_d::ScalarTestDomain, target_symbol, source_symbol)
     # This domain always interacts with the single cell in instances of itself, and nothing else
-    (target = 1, source = 1, target_unit = Cells(), source_unit = Cells())
+    (target = [1], source = [1], target_unit = Cells(), source_unit = Cells())
 end
 
 # Driving force for the test equation
