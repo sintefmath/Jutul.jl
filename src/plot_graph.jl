8101adb34553fc485ff38631262193d28f49dce7
Martin Johnsrud <martin.johnsrud@gmail.com>
2021-07-11 01:31:33 +0200

    added tool for ploting dependency

---
+++ src/plot_graph.jl
@@ -0,0 +1,41 @@
+using Terv
+using Plots, GraphRecipes
+
+export plot_graph
+
+
+function get_graph(model)
+    primary = model.primary_variables
+    secondary = model.secondary_variables
+    nodes, edges = build_variable_graph(model, primary, secondary)
+    return nodes, edges
+end
+
+function get_index_graph(nodes, edges)
+    nodes_num = 1:size(nodes)[1]
+    nodes_ind = Dict([nodes[i] => i for i in nodes_num])
+    edges_ind = Vector{Vector{Any}}()
+
+    for edges_vec in edges
+        v = []
+        for d in edges_vec
+            push!(v, nodes_ind[d])
+        end
+        push!(edges_ind, v)
+    end
+    
+    return nodes_ind, edges_ind
+end
+
+function plot_graph(model)
+    nodes = [:a, :b, :c, :d]
+    edges = [[], [:a], [:a], [:a, :c]]
+
+    nodes, edges = get_graph(model)
+    print(nodes)
+    print(edges)
+    nodes_ind, edges_ind = get_index_graph(nodes, edges)
+
+    graphplot(edges_ind)
+end
+
