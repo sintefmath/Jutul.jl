e73eb3262e93874a5331f064be4a702ba9235171
Olav Møyner <olav.moyner@gmail.com>
2021-07-29 11:25:21 +0200

    Some work on making the IterativeSolvers krylov solves work

--- src/linsolve/block_cpu.jl
+++ src/linsolve/block_cpu.jl
@@ -1,27 +1,32 @@
+function block_mul!(res, jac, Vt, x, α, β::T) where T
+    as_svec = (x) -> reinterpret(Vt, x)
+    res_v = as_svec(res)
+    x_v = as_svec(x)
+    if β == zero(T)
+        mul!(res_v, jac, x_v)
+        if α != one(T)
+            lmul!(α, res_v)
+        end
+    else
+        # TODO: optimize me like the three argument version.
+        res_v .= α.*jac*x_v + β.*res_v
+    end
+end
+
+
+function ldiv!(TervPrecond, x)
+
+end
+
 function get_mul!(sys::LinearizedSystem{BlockMajorLayout})
     jac = sys.jac
 
     Vt = eltype(sys.r)
     # Mt = eltype(jac)
-
-    as_svec = (x) -> reinterpret(Vt, x)
     # as_smat = (x) -> reinterpret(Mt, x)
     # as_float = (x) -> reinterpret(Float64, x)
 
-    function block_mul!(res, x, α, β::T) where T
-        res_v = as_svec(res)
-        x_v = as_svec(x)
-        if β == zero(T)
-            mul!(res_v, jac, x_v)
-            if α != one(T)
-                lmul!(α, res_v)
-            end
-        else
-            # TODO: optimize me like the three argument version.
-            res_v .= α.*jac*x_v + β.*res_v
-        end
-    end
-    return block_mul!
+    return (res, x, α, β) -> block_mul!(res, jac, Vt, x, α, β)
 end
 
 function vector_residual(sys::LinearizedSystem{BlockMajorLayout})
--- src/linsolve/krylov.jl
+++ src/linsolve/krylov.jl
@@ -1,4 +1,20 @@
+
+
 export GenericKrylov
+
+struct PrecondWrapper
+    op::LinearOperator
+end
+
+eltype(p::PrecondWrapper) = eltype(p.op)
+
+mul!(x, p::PrecondWrapper, arg...) = mul!(x, p.op, arg...)
+
+function LinearAlgebra.ldiv!(p::PrecondWrapper, x)
+    y = copy(x)
+    mul!(x, p.op, y, 1, 0) 
+    # lmul!(x, p.op, y)
+end
 
 mutable struct GenericKrylov
     solver
@@ -29,7 +45,7 @@ function preconditioner(krylov::GenericKrylov, sys, arg...)
         op = I
     else
         update!(M, sys)
-        op = linear_operator(M, arg...)
+        op = PrecondWrapper(linear_operator(M, arg...))
     end
     return op
 end
@@ -47,20 +63,31 @@ function solve!(sys::LSystem, krylov::GenericKrylov)
     max_it = cfg.max_iterations
     rt = rtol(cfg)
     at = atol(cfg)
-    (x, stats) = solver(op, r, 
-                            itmax = max_it,
-                            verbose = v,
-                            rtol = rt,
-                            history = true,
-                            atol = at,
-                            M = L, N = R)
-    res = stats.residuals
-    n = length(res) - 1
-    if !stats.solved
-        @warn "Linear solver: $(stats.status), final residual: $(res[end]). rtol = $rt, atol = $at, max_it = $max_it"
-    end
-    if v > 0
-        @debug "Final residual $(res[end]), rel. value $(res[end]/res[1]) after $n iterations."
+    if from_IterativeSolvers(solver)
+        # Pl = krylov.preconditioner.factor
+        (x, history) = solver(op, r, abstol = at, reltol = rt, log = true, Pl = L, maxiter = max_it, verbose = v > 0)#, Pl = L, Pr = R, )
+        display(history)
+    else
+        (x, stats) = solver(op, r, 
+                                itmax = max_it,
+                                verbose = v,
+                                rtol = rt,
+                                history = true,
+                                atol = at,
+                                M = L, N = R)
+        res = stats.residuals
+        n = length(res) - 1
+        if !stats.solved
+            @warn "Linear solver: $(stats.status), final residual: $(res[end]). rtol = $rt, atol = $at, max_it = $max_it"
+        end
+        if v > 0
+            @debug "Final residual $(res[end]), rel. value $(res[end]/res[1]) after $n iterations."
+        end
     end
     update_dx_from_vector!(sys, x)
+end
+
+
+function from_IterativeSolvers(f)
+    return f == gmres
 end
