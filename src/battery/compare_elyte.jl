82907eccb168301eedb092814690aec5d3b2f02e
Martin Johnsrud <martin.johnsrud@gmail.com>
2021-08-11 15:36:41 +0200

    added comparison bw 2D simple elyte

--- src/battery/compare_elyte.jl
+++ src/battery/compare_elyte.jl
@@ -55,10 +55,11 @@ function test_simple_elyte()
     cfg = simulator_config(sim)
     cfg[:linear_solver] = nothing
     states = simulate(sim, timesteps, config = cfg)
-    return states, G, model
+    rs = exported["states"][:, 1]
+    return states, G, model, rs
 end
 
-states, G, model = test_simple_elyte();
+states, G, model, rs = test_simple_elyte();
 
 ##
 
@@ -67,7 +68,7 @@ display(f)
 ##
 
 accstates = []
-for i in 1:18
+for i in 1:19
     state = Dict{Symbol, Any}()
     state[:MassAcc] = (states[i+1][:MassAcc] .- states[i][:MassAcc])
     push!(accstates, state)
@@ -76,3 +77,36 @@ end
 f = plot_interactive(G, accstates)
 display(f)
 ##
+
+j2m = Dict{Symbol, String}(
+    :C                  => "cs",
+    :T                  => "T",
+    :Phi                => "phi", 
+    :Conductivity       => "conductivity",
+    :Diffusivity        => "D",
+    :TotalCurrent       => "j",
+    :ChargeCarrierFlux  => "LiFlux" 
+)
+ref_states = get_ref_states(j2m, rs);
+
+##
+f = plot_interactive(G, ref_states)
+display(f)
+
+
+##
+
+function print_diff(s, sref, n, k)
+    Δ = abs.(1 .- s[n][k] ./ sref[n][k])
+    println("Δ = $(maximum(Δ))")
+end
+
+ks = (:C, :Phi)
+
+for n in 1:20
+    println("Step $n")
+    for k in ks
+        println(k)
+        print_diff(states, ref_states, n, k)
+    end
+end
