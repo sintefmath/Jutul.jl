263f1161a89adcd8a0b95cd3ff4376f57aa0c58e
Martin Johnsrud <martin.johnsrud@gmail.com>
2021-08-13 13:44:20 +0200

    fixed error in cct model test

--- src/battery/test_cc_temp.jl
+++ src/battery/test_cc_temp.jl
@@ -12,7 +12,7 @@ ENV["JULIA_DEBUG"] = Terv;
 function test_ccT(name="square_current_collector")
     bc=[1, 9]
     b_T_hf=[2., 2.]
-    domain, exported = get_cc_grid(name=name, extraout=true, bc=bc, b_T_hf=b_T_hf)
+    domain, exported = get_cc_grid(name=name, extraout=true, bc=bc, b_T_hf=b_T_hf, tensor_map=true)
     time = 1e2
     t = LinRange(0, time, 10)
     timesteps = diff(t)
--- src/battery/test_setup.jl
+++ src/battery/test_setup.jl
@@ -89,7 +89,7 @@ function exported_model_to_domain(exported; bc=[], b_T_hf=[])
     G = MinimalECTPFAGrid(volumes, N, bc, b_T_hf, P, S)
 
     T = exported["operators"]["T"].*2.0
-    flow = TPFlow(G, T)
+    flow = TPFlow(G, T, tensor_map=tensor_map)
     disc = (charge_flow = flow,)
     D = DiscretizedDomain(G, disc)
 
