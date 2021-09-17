77f0fc75b4df3d2ebdf8a87155c219e0b6e690c6
Olav Møyner <olav.moyner@gmail.com>
2021-09-17 11:53:19 +0200

    Add 2D plotting of Cart mesh

--- src/meshes/cart.jl
+++ src/meshes/cart.jl
@@ -43,14 +43,30 @@ Lower corner for one dimension, without any transforms applied
 coord_offset(pos, δ::AbstractFloat) = (pos-1)*δ
 coord_offset(pos, δ::AbstractVector) = sum(δ[1:(pos-1)])
 
+"""
+Linear index of Cartesian mesh cell
+"""
+function cell_index(g, pos)
+    nx, ny, nz = get_3d_dims(g)
+    x, y, z = get_3d_pos(g, pos)
+    return (z-1)*nx*ny + (y-1)*nx + x
+end
+
+"""
+Cell dimensions (as 3 tuple)
+"""
+function cell_dims(g, pos)
+    x, y, z = get_3d_pos(g, pos)
+    Δ = g.deltas
+    return (get_delta(Δ, x, 1), get_delta(Δ, y, 2), get_delta(Δ, z, 3))
+end
+
 function tpfv_geometry(g::CartesianMesh)
     Δ = g.deltas
     d = dim(g)
 
     nx, ny, nz = get_3d_dims(g)
 
-    cell_index(x, y, z) = (z-1)*nx*ny + (y-1)*nx + x
-    cell_dims(x, y, z) = (get_delta(Δ, x, 1), get_delta(Δ, y, 2), get_delta(Δ, z, 3))
     # Cell data first - volumes and centroids
     nc = nx*ny*nz
     V = zeros(nc)
@@ -59,8 +75,8 @@ function tpfv_geometry(g::CartesianMesh)
         for y in 1:ny
             for z = 1:nz
                 pos = (x, y, z)
-                c = cell_index(pos...)
-                cdim  = cell_dims(pos...)
+                c = cell_index(g, pos)
+                cdim  = cell_dims(g, pos)
                 V[c] = prod(cdim)
 
                 for i in 1:d
@@ -78,10 +94,11 @@ function tpfv_geometry(g::CartesianMesh)
     face_normals = zeros(d, nf)
 
     function add_face!(face_areas, face_normals, face_centroids, x, y, z, D, pos)
-        index = cell_index(x, y, z)
+        t = (x, y, z)
+        index = cell_index(g, t)
         N[1, pos] = index
-        N[2, pos] = cell_index(x + (D == 1), y + (D == 2), z + (D == 3))
-        Δ  = cell_dims(x, y, z)
+        N[2, pos] = cell_index(g, (x + (D == 1), y + (D == 2), z + (D == 3)))
+        Δ  = cell_dims(g, t)
         # Face area
         A = 1
         for i in setdiff(1:3, D)
@@ -142,6 +159,25 @@ function get_3d_dims(g)
     return (nx, ny, nz)
 end
 
+function get_3d_pos(g, t::Tuple)
+    d = length(t)
+    if d == 1
+        nx = t[1]
+        ny = nz = 1
+    elseif d == 2
+        nx, ny = t
+        nz = 1
+    else
+        @assert d == 3
+        nx, ny, nz = t
+    end
+    return (nx, ny, nz)
+end
+
+function get_3d_pos(g, t::Integer)
+    error("Not implemented")
+end
+
 
 function get_delta(Δ, index, d)
     if length(Δ) >= d
@@ -161,19 +197,31 @@ end
 function triangulate_outer_surface(m::CartesianMesh, is_depth = true)
     pts = []
     tri = []
-    cell_index = []
+    cell_ix = []
     face_index = []
     offset = 0
     d = dim(m)
-    nc = number_of_cells(m)
+    # nc = number_of_cells(m)
     if d == 2
-        cell_index = 1:nc
-        for i = 1:nc
-            local_pts = []
-            local_tri = [1 2 3; 3 4 1]
-            push!(pts, local_pts)
-            push!(tri, local_tri .+ offset)
-            offset += 4
+        nx, ny, = m.dims
+        Δ = m.deltas
+        for x = 1:nx
+            for y = 1:ny
+                t = (x, y, 1)
+                dx, dy,  = cell_dims(m, t)
+                x0 = coord_offset(x, Δ[1])
+                y0 = coord_offset(y, Δ[2])
+
+                local_pts = [x0      y0;
+                             x0 + dx y0;
+                             x0 + dx y0 + dy
+                             x0      y0 + dy]
+                local_tri = [1 2 3; 3 4 1]
+                push!(pts, local_pts)
+                push!(tri, local_tri .+ offset)
+                push!(cell_ix, repeat([cell_index(m, t)], 4))
+                offset += 4
+            end
         end
     else
         @assert d == 3
@@ -182,13 +230,13 @@ function triangulate_outer_surface(m::CartesianMesh, is_depth = true)
     pts = vcat(pts...)
     tri = vcat(tri...)
 
-    cell_index = vcat(cell_index...)
+    cell_ix = vcat(cell_ix...)
     face_index = vcat(face_index...)
 
     mapper = (
-                Cells = (cell_data) -> cell_data[cell_index],
+                Cells = (cell_data) -> cell_data[cell_ix],
                 Faces = (face_data) -> face_data[face_index],
-                indices = (Cells = cell_index, Faces = face_index)
+                indices = (Cells = cell_ix, Faces = face_index)
               )
     return (pts, tri, mapper)
 end
