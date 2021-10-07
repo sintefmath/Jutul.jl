b20f7f0e23f46d3e26b73222c48dfe42d384cb3e
Olav Møyner <olav.moyner@gmail.com>
2021-10-07 18:42:39 +0200

    Update multicomponent_secondary_variables.jl

--- src/applications/reservoir_simulator/multicomponent_secondary_variables.jl
+++ src/applications/reservoir_simulator/multicomponent_secondary_variables.jl
@@ -131,7 +131,11 @@ degrees_of_freedom_per_entity(model, v::MassMobilities) = number_of_phases(model
 end
 
 @terv_secondary function update_as_secondary!(Sat, s::Saturations, model::SimulationModel{D, S}, param, FlashResults) where {D, S<:CompositionalSystem}
-    # error()
+    for i in 1:size(Sat, 2)
+        S_l, S_v = phase_saturations(FlashResults[i])
+        Sat[1, i] = S_l
+        Sat[2, i] = S_v
+    end
 end
 
 @terv_secondary function update_as_secondary!(massmob, m::MassMobilities, model::SimulationModel{D, S}, param) where {D, S<:CompositionalSystem}
