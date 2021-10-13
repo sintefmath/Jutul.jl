42fa0f3b0f3838f2f2489d1e4a2d3c092de7c106
Olav Møyner <olav.moyner@gmail.com>
2021-10-13 10:52:35 +0200

    source term treatment for compositional

--- src/applications/reservoir_simulator/multicomponent.jl
+++ src/applications/reservoir_simulator/multicomponent.jl
@@ -35,3 +35,58 @@ end
 #     np = number_of_partials_per_entity(model, Cells())
 #     storage[:flash] = flash_storage(eos, c, m, inc_jac = true, diff_externals = true, npartials = np)
 # end
+
+
+function apply_forces_to_equation!(storage, model::SimulationModel{D, S}, eq::ConservationLaw, force::V, time) where {V <: AbstractVector{SourceTerm{I, F, T}}, D, S<:TwoPhaseCompositionalSystem} where {I, F, T}
+    acc = get_diagonal_entries(eq)
+    state = storage.state
+    kr = state.RelativePermeabilities
+    rho = state.PhaseMassDensities
+    mu = state.PhaseViscosities
+    X = state.LiquidMassFractions
+    Y = state.VaporMassFractions
+    rhoS = get_reference_densities(model, storage)
+    insert_component_sources!(acc, kr, mu, X, Y, rho, rhoS, force)
+end
+
+function insert_component_sources!(acc, kr, mu, X, Y, rho, rhoS, sources)
+    ncomp = size(acc, 1)
+    for src in sources
+        for c = 1:ncomp
+            @inbounds acc[c, src.cell] -= component_source(src, kr, mu, X, Y, rho, rhoS, c)
+        end
+    end
+end
+
+function component_source(src, arg...)
+    # Treat inflow as volumetric with respect to surface conditions
+    # Treat outflow as volumetric sources
+    v = src.value
+    cell = src.cell
+    if v > 0
+        q = in_component_source(src, arg..., v, cell)
+    else
+        q = out_component_source(src, arg..., v, cell)
+    end
+    return q
+end
+
+function in_component_source(src, kr, mu, X, Y, rho, rhoS, c, v, cell)
+    f = src.fractional_flow[c]
+    return v*f
+end
+
+function out_component_source(src, kr, mu, X, Y, rho, rhoS, c, v, cell)
+    λ_l = local_mobility(kr, mu, 1, cell)
+    λ_v = local_mobility(kr, mu, 1, cell)
+    λ_t = λ_l + λ_v
+
+    ρ_l = rho[1, cell]
+    ρ_v = rho[2, cell]
+
+    x = X[c, cell]
+    y = Y[c, cell]
+
+    f = (λ_l/λ_t)*x*ρ_l + (λ_v/λ_t)*y*ρ_v
+    return v*f
+end
