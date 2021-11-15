764f30c4f4024ba381f8194abc1fed7495a2d361
Olav Møyner <olav.moyner@gmail.com>
2021-11-15 14:49:46 +0100

    Krylov: Avoid warnings for verbosity < 0

--- src/linsolve/krylov.jl
+++ src/linsolve/krylov.jl
@@ -125,7 +125,7 @@ function solve!(sys::LSystem, krylov::GenericKrylov, model, storage = nothing, d
         final_res = norm(op*x - r)
     end
 
-    if !solved
+    if !solved && v >= 0
         @warn "Linear solver: $msg, final residual: $final_res, rel. value $(final_res/initial_res). rtol = $rt, atol = $at, max_it = $max_it"
     elseif v > 0 
         @debug "$n lsolve its: Final residual $final_res, rel. value $(final_res/initial_res)."
