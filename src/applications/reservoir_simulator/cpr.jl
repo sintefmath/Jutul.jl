cf765ed916be9f405bd0c94f617f3a9faf93953a
Olav Møyner <olav.moyner@gmail.com>
2021-12-20 20:08:09 +0100

    Gen code for CPR true impes > 2 c

--- src/applications/reservoir_simulator/cpr.jl
+++ src/applications/reservoir_simulator/cpr.jl
@@ -165,8 +165,14 @@ end
 
 function true_impes!(w, acc, r, n, bz, arg...)
     if bz == 2
-        # Hard coded variant
+        # Hard coded variants
         true_impes_2!(w, acc, r, n, bz, arg...)
+    elseif bz == 3
+        true_impes_3!(w, acc, r, n, bz, arg...)
+    elseif bz == 4
+        true_impes_4!(w, acc, r, n, bz, arg...)
+    elseif bz == 5
+        true_impes_5!(w, acc, r, n, bz, arg...)
     else
         true_impes_gen!(w, acc, r, n, bz, arg...)
     end
@@ -190,6 +196,45 @@ function true_impes_2!(w, acc, r, n, bz, p_scale, scaling)
     end
 end
 
+function M_entry(acc, i, j, c)
+    return acc[i, c].partials[j]
+end
+
+function true_impes_3!(w, acc, r, n, bz, s, scaling)
+    r_p = SVector{3}(r)
+    f(i, j, c) = M_entry(acc, i, j, c)
+    for c in 1:n
+        A = @SMatrix    [s*f(1, 1, c) f(1, 2, c) f(1, 3, c);
+                         s*f(2, 1, c) f(2, 2, c) f(2, 3, c);
+                         s*f(3, 1, c) f(3, 2, c) f(3, 3, c);]
+        invert_w!(w, A, r_p, c, bz, scaling)
+    end
+end
+
+function true_impes_4!(w, acc, r, n, bz, s, scaling)
+    r_p = SVector{4}(r)
+    f(i, j, c) = M_entry(acc, i, j, c)
+    for c in 1:n
+        A = @SMatrix    [s*f(1, 1, c) f(1, 2, c) f(1, 3, c) f(1, 4, c);
+                         s*f(2, 1, c) f(2, 2, c) f(2, 3, c) f(2, 4, c);
+                         s*f(3, 1, c) f(3, 2, c) f(3, 3, c) f(3, 4, c);
+                         s*f(4, 1, c) f(4, 2, c) f(4, 3, c) f(4, 4, c)]
+        invert_w!(w, A, r_p, c, bz, scaling)
+    end
+end
+
+function true_impes_5!(w, acc, r, n, bz, s, scaling)
+    r_p = SVector{5}(r)
+    f(i, j, c) = M_entry(acc, i, j, c)
+    for c in 1:n
+        A = @SMatrix    [s*f(1, 1, c) f(1, 2, c) f(1, 3, c) f(1, 4, c) f(1, 5, c);
+                         s*f(2, 1, c) f(2, 2, c) f(2, 3, c) f(2, 4, c) f(2, 5, c);
+                         s*f(3, 1, c) f(3, 2, c) f(3, 3, c) f(3, 4, c) f(3, 5, c);
+                         s*f(4, 1, c) f(4, 2, c) f(4, 3, c) f(4, 4, c) f(4, 5, c);
+                         s*f(5, 1, c) f(5, 2, c) f(5, 3, c) f(5, 4, c) f(5, 5, c)]
+        invert_w!(w, A, r_p, c, bz, scaling)
+    end
+end
 
 function true_impes_gen!(w, acc, r, n, bz, p_scale, scaling)
     r_p = SVector{bz}(r)
