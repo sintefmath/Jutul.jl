fa908e5edfdaaedbfc515b7ef819a6c2ecaf0ae2
Olav Møyner <olav.moyner@gmail.com>
2022-01-02 21:04:03 +0100

    Update multicomponent_secondary_variables.jl

--- src/applications/reservoir_simulator/multicomponent_secondary_variables.jl
+++ src/applications/reservoir_simulator/multicomponent_secondary_variables.jl
@@ -278,8 +278,9 @@ end
 
 @terv_secondary function update_as_secondary!(mu, m::LBCViscosities, model::SimulationModel{D, S}, param, Pressure, Temperature, FlashResults) where {D, S<:CompositionalSystem}
     eos = model.system.equation_of_state
+    n = size(mu, 2)
     tb = thread_batch(model.context)
-    @inbounds @batch minbatch = tb for i in 1:size(mu, 2)
+    @inbounds @batch minbatch = tb for i in 1:n
         p = Pressure[i]
         T = Temperature[1, i]
         mu[1, i], mu[2, i] = lbc_viscosities(eos, p, T, FlashResults[i])
