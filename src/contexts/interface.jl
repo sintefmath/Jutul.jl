7a33e41b3b4debe8aaf7c08400caad76a8ebd09a
Olav Møyner <olav.moyner@gmail.com>
2022-05-25 11:30:39 +0200

    Generalize block size == 1 -> Float64

--- src/contexts/interface.jl
+++ src/contexts/interface.jl
@@ -10,11 +10,23 @@ function r_eltype(context, layout, block_size)
 end
 
 function jacobian_eltype(context::CPUJutulContext, layout::BlockMajorLayout, block_size)
-    return SMatrix{block_size..., float_type(context), prod(block_size)}
+    F = float_type(context)
+    if block_size[1] == block_size[2] == 1
+        M = Float64
+    else
+        M = SMatrix{block_size..., F, prod(block_size)}
+    end
+    return M
 end
 
 function r_eltype(context::CPUJutulContext, layout::BlockMajorLayout, block_size)
-    return SVector{block_size[1], float_type(context)}
+    F = float_type(context)
+    if block_size == 1
+        V = F
+    else
+        V = SVector{block_size, F}
+    end
+    return V
 end
 
 function build_sparse_matrix(context, I, J, V, n, m)
