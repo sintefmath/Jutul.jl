db5d643cef73c4f41077adec5eceb75b1f1d5df5
Olav Møyner <olav.moyner@gmail.com>
2022-06-13 20:27:37 +0200

    split scalar test system into manual part

---
+++ src/applications/test_systems/manual.jl
@@ -0,0 +1,28 @@
+# Manual sparsity version
+function setup_equation_storage(model, e::ScalarTestEquation{ManualTestDisc}; kwarg...)
+    Ω = model.domain
+    nc = number_of_cells(Ω)
+    @assert nc == 1 # We use nc for clarity of the interface - but it should always be one!
+    ne = 1 # Single, scalar equation
+    npartials = number_of_equations_per_entity(e)
+    e = CompactAutoDiffCache(ne, nc, npartials, context = model.context; kwarg...)
+    return e
+end
+
+function declare_pattern(model, e::ScalarTestEquation{ManualTestDisc}, eq_storage, unit)
+    @assert unit == Cells()
+    return ([1], [1])
+end
+
+function update_equation!(eq_s::CompactAutoDiffCache, eq::ScalarTestEquation{ManualTestDisc}, storage, model, dt)
+    X = storage.state.XVar
+    X0 = storage.state0.XVar
+    equation = get_entries(eq_s)
+    @. equation = (X - X0)/dt
+end
+
+function apply_forces_to_equation!(storage, model, eq::ScalarTestEquation{ManualTestDisc}, eq_s, force::ScalarTestForce, time)
+    equation = get_entries(eq_s)
+    @. equation -= force.value
+end
+
--- src/applications/test_systems/test_systems.jl
+++ src/applications/test_systems/test_systems.jl
@@ -28,55 +28,23 @@ struct AutoTestDisc <: AbstractTestDisc end
 struct ScalarTestEquation{D} <: DiagonalEquation
     discretization::D
     function ScalarTestEquation(domain, system, formulation)
-        # Ω = model.domain
-        # nc = number_of_cells(Ω)
         if domain.use_manual
             D = ManualTestDisc()
         else
             D = AutoTestDisc()
         end
-        # @assert nc == 1 # We use nc for clarity of the interface - but it should always be one!
-        # ne = 1 # Single, scalar equation
-        # e = CompactAutoDiffCache(ne, nc, npartials, context = context; kwarg...)
         new{typeof(D)}(D)
     end
 end
 
 number_of_equations_per_entity(::ScalarTestEquation) = 1
 
-function setup_equation_storage(model, e::ScalarTestEquation{ManualTestDisc}; kwarg...)
-    Ω = model.domain
-    nc = number_of_cells(Ω)
-    @assert nc == 1 # We use nc for clarity of the interface - but it should always be one!
-    ne = 1 # Single, scalar equation
-    npartials = number_of_equations_per_entity(e)
-    e = CompactAutoDiffCache(ne, nc, npartials, context = model.context; kwarg...)
-    return e
-end
-
-function declare_pattern(model, e::ScalarTestEquation{ManualTestDisc}, eq_storage, unit)
-    @assert unit == Cells()
-    return ([1], [1])
-end
-
 function select_equations_system!(eqs, domain, system::ScalarTestSystem, formulation)
     eqs[:test_equation] = ScalarTestEquation(domain, system, formulation)
 end
 
-function update_equation!(eq_s::CompactAutoDiffCache, eq::ScalarTestEquation, storage, model, dt)
-    X = storage.state.XVar
-    X0 = storage.state0.XVar
-    equation = get_entries(eq_s)
-    @. equation = (X - X0)/dt
-end
-
 function setup_forces(model::SimulationModel{G, S}; sources = nothing) where {G<:ScalarTestDomain, S<:ScalarTestSystem}
     return (sources = sources,)
-end
-
-function apply_forces_to_equation!(storage, model, eq::ScalarTestEquation{ManualTestDisc}, eq_s, force::ScalarTestForce, time)
-    equation = get_entries(eq_s)
-    @. equation -= force.value
 end
 
 function update_cross_term!(ct::InjectiveCrossTerm, eq::ScalarTestEquation, target_storage, source_storage, target_model, source_model, target, source, dt)
@@ -96,3 +64,5 @@ struct XVar <: ScalarVariable end
 function select_primary_variables!(S, domain, system::ScalarTestSystem, formulation)
     S[:XVar] = XVar()
 end
+
+include("manual.jl")
