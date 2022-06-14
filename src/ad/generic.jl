e1e403532881ff0def0a764b146db55a926be8a8
Olav Møyner <olav.moyner@gmail.com>
2022-06-14 21:58:40 +0200

    Update generic.jl

--- src/ad/generic.jl
+++ src/ad/generic.jl
@@ -20,9 +20,29 @@ number_of_partials(c::GenericAutoDiffCache{N, E, ∂T}) where {N, E, ∂T} = num
 vrange(c::GenericAutoDiffCache, i) = c.vpos[i]:(c.vpos[i+1]-1)
 get_entries(c::GenericAutoDiffCache) = c.entries
 
-@inline function get_jacobian_pos(c::GenericAutoDiffCache, index, eqNo, partial_index, pos)
+@inline function get_jacobian_pos(c::GenericAutoDiffCache, index, eqNo, partial_index, pos = c.jacobian_positions)
     np = number_of_partials(c)
     @inbounds pos[(eqNo-1)*np + partial_index, index]
 end
 
 diagonal_view(cache::GenericAutoDiffCache) = view(cache.entries, :, cache.diagonal_positions)
+
+function fill_equation_entries!(nz, r, model, cache::GenericAutoDiffCache)
+    nu, ne, np = ad_dims(cache)
+    entries = cache.entries
+    tb = minbatch(model.context)
+    # @batch minbatch = tb for i in 1:nu
+    for i in 1:nu
+        for (jno, j) in enumerate(vrange(cache, i))
+            for e in 1:ne
+                a = entries[e, j]
+                if jno == 1
+                    insert_residual_value(r, i + nu*(e-1), a.value)
+                end
+                for d = 1:np
+                    update_jacobian_entry!(nz, cache, j, e, d, a.partials[d])
+                end
+            end
+        end
+    end
+end
