bd86b67ce84ba0e8962e9a1cb27a6183aac4822e
Olav Møyner <olav.moyner@gmail.com>
2022-06-14 21:58:44 +0200

    Update heat_2d.jl

--- test/test_systems/heat_2d.jl
+++ test/test_systems/heat_2d.jl
@@ -1,10 +1,10 @@
 using Jutul
 using Test
 
-function test_poisson(n = 10)
+function test_poisson(nx = 3, ny = nx)
     sys = SimpleHeatSystem()
     # Unit square
-    g = CartesianMesh((10, 10), (1.0, 1.0))
+    g = CartesianMesh((nx, ny), (1.0, 1.0))
     D = DiscretizedDomain(g)
     model = SimulationModel(D, sys)
     
@@ -19,5 +19,5 @@ function test_poisson(n = 10)
 end
 
 @testset "Poisson 2D" begin
-    @test test_poisson()
+    @test test_poisson(3, 1)
 end
