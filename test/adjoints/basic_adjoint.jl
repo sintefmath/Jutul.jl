6dd2036b86889d0c51c199686bcb965ace6243b9
Olav Møyner <olav.moyner@sintef.no>
2022-08-16 11:56:52 +0200

    Add tests for in-place adjoint interface

--- test/adjoints/basic_adjoint.jl
+++ test/adjoints/basic_adjoint.jl
@@ -6,7 +6,7 @@ function poisson_test_objective(model, state)
 end
 
 
-function mytest(; nx = 3, ny = 1, dt = [1.0, 2.0, π], extra_timing = false)
+function mytest(; nx = 3, ny = 1, dt = [1.0, 2.0, π], in_place = false, extra_timing = false)
     sys = VariablePoissonSystem(time_dependent = true)
     # Unit square
     g = CartesianMesh((nx, ny), (1.0, 1.0))
@@ -16,7 +16,8 @@ function mytest(; nx = 3, ny = 1, dt = [1.0, 2.0, π], extra_timing = false)
     model = SimulationModel(D, sys)
     # Initial condition doesn't matter
     state0 = setup_state(model, Dict(:U=>1.0))
-    param = setup_parameters(model, K = compute_face_trans(g, 1.0))
+    K = compute_face_trans(g, 1.0)
+    param = setup_parameters(model, K = K)
 
     nc = number_of_cells(g)
     pos_src = PoissonSource(1, 1.0)
@@ -28,8 +29,15 @@ function mytest(; nx = 3, ny = 1, dt = [1.0, 2.0, π], extra_timing = false)
 
     # Define objective
     G = (model, state, dt, step_no, forces) -> poisson_test_objective(model, state)
-    grad_adj = Jutul.solve_adjoint_sensitivities(model, states, reports, G,
-                        forces = forces, state0 = state0, parameters = param, extra_timing = extra_timing)
+    if in_place
+        grad_adj = zeros(length(K))
+        storage = Jutul.setup_adjoint_storage(model; state0 = state0, parameters = param)
+        grad_adj = Jutul.solve_adjoint_sensitivities!(grad_adj, storage, states, state0, dt, G,
+                            forces = forces)
+    else
+        grad_adj = Jutul.solve_adjoint_sensitivities(model, states, reports, G,
+                            forces = forces, state0 = state0, parameters = param, extra_timing = extra_timing)
+    end
     # Check against numerical gradient
     grad_num = Jutul.solve_numerical_sensitivities(model, states, reports, G, :K,
                             forces = forces, state0 = state0, parameters = param)
@@ -37,8 +45,10 @@ function mytest(; nx = 3, ny = 1, dt = [1.0, 2.0, π], extra_timing = false)
 end
 
 @testset "simple adjoint sensitivities" begin
-    # Test single step since it hits less of the code
-    mytest(dt = [1.0])
-    # Test with multiple time-steps
-    mytest()
+    for in_place in [false, true]
+        # Test single step since it hits less of the code
+        mytest(dt = [1.0], in_place = in_place)
+        # Test with multiple time-steps
+        mytest(in_place = in_place)
+    end
 end
