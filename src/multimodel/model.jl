9e21ea0f0fdcd4930c76746b4dba1a6b15cfeb76
hnil <Halvor.M.Nilsen@sintef.no>
2022-08-24 11:49:07 +0200

    Fix missing BC

--- src/multimodel/model.jl
+++ src/multimodel/model.jl
@@ -375,6 +375,8 @@ function update_equations_and_apply_forces!(storage, model::MultiModel, dt, forc
     @timeit "crossterm update" update_cross_terms!(storage, model, dt)
     # Apply forces
     @timeit "forces" apply_forces!(storage, model, dt, forces; time = time)
+    # Boundary conditions
+    @timeit "boundary conditions" apply_boundary_conditions!(storage, model)
     # Apply forces to cross-terms
     @timeit "crossterm forces" apply_forces_to_cross_terms!(storage, model, dt, forces; time = time)
 end
