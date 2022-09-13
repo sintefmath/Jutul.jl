93c44349bbaab0661472bb427f83b10338c78a8f
Olav Møyner <olav.moyner@gmail.com>
2022-09-13 13:59:20 +0200

    Update optimization.jl

--- src/simulator/optimization.jl
+++ src/simulator/optimization.jl
@@ -1,5 +1,5 @@
 
-export update_objective_new_parameters!
+export update_objective_new_parameters!, setup_parameter_optimization
 
 function update_objective_new_parameters!(param_serialized, sim, state0, param, forces, dt, G; log_obj = false, config = nothing, kwarg...)
     if isnothing(config)
@@ -11,5 +11,34 @@ function update_objective_new_parameters!(param_serialized, sim, state0, param, 
     if log_obj
         println("Current objective: $obj")
     end
-    return obj
+    return (obj, states)
+end
+
+function setup_parameter_optimization(model, state0, param, dt, forces, G; grad_type = :adjoint, config = nothing, kwarg...)
+    x0 = vectorize_variables(model, param, :parameters)
+    data = Dict()
+
+    sim = Simulator(model, state0 = state0, parameters = param)
+    if isnothing(config)
+        config = simulator_config(sim; info_level = -1, kwarg...)
+    end
+
+    function F(x)
+        devectorize_variables!(param, sim.model, x, :parameters)
+        states, = simulate(state0, sim, dt, parameters = param, forces = forces, config = config)
+        data[:states] = states
+        obj = evaluate_objective(G, sim.model, states, dt, forces)
+        println("Current objective: $obj")
+        return obj
+    end
+    @assert grad_type == :adjoint
+    grad_adj = similar(x0)
+    data[:grad_adj] = grad_adj
+    function dF(x)
+        # TODO: Avoid re-allocating storage.
+        devectorize_variables!(param, model, x, :parameters)
+        storage = setup_adjoint_storage(model, state0 = state0, parameters = param)
+        return solve_adjoint_sensitivities!(grad_adj, storage, data[:states], state0, dt, G, forces = forces)
+    end
+    return (F, dF, x0, data)
 end
