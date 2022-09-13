4ace98bcc0fa6f4298db7b7e364ff599dfcb13ac
Olav Møyner <olav.moyner@gmail.com>
2022-09-13 10:07:37 +0200

    Update optimization.jl

--- src/simulator/optimization.jl
+++ src/simulator/optimization.jl
@@ -3,6 +3,6 @@ export update_objective_new_parameters!
 
 function update_objective_new_parameters!(param_serialized, sim, state0, param, forces, dt, G; config = simulator_config(sim), kwarg...)
     devectorize_variables!(param, sim.model, param_serialized, :parameters)
-    states, = simulate(state0, sim, dt, parameters = param, forces = forces, kwarg...)
+    states, = simulate(state0, sim, dt, parameters = param, forces = forces; kwarg...)
     return evaluate_objective(G, sim.model, states, dt, forces)
 end
