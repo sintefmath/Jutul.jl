f8e74b4a41679b43364b4a2f97955daa02677f0a
Olav Møyner <olav.moyner@gmail.com>
2022-09-14 15:05:18 +0200

    Split vectorization into inner routine

--- src/multimodel/vectorization.jl
+++ src/multimodel/vectorization.jl
@@ -1,25 +1,35 @@
-function vectorize_variables(model::MultiModel, state_or_prm, type_or_map = :primary)
+function vectorize_variables(model::MultiModel, state_or_prm, type_or_map = :primary; config = nothing)
     mapper = get_mapper_internal(model, type_or_map)
     n = 0
     for (k, v) in mapper
         n += sum(x -> x.n, values(v), init = 0)
     end
     V = zeros(n)
-    vectorize_variables!(V, model, state_or_prm, mapper)
+    vectorize_variables!(V, model, state_or_prm, mapper, config = config)
 end
 
-function vectorize_variables!(V, model::MultiModel, state_or_prm, type_or_map = :primary)
+function vectorize_variables!(V, model::MultiModel, state_or_prm, type_or_map = :primary; config = nothing)
     mapper = get_mapper_internal(model, type_or_map)
     for (k, submodel) in pairs(model.models)
-        vectorize_variables!(V, submodel, state_or_prm[k], mapper[k])
+        if isnothing(config)
+            c = nothing
+        else
+            c = config[k]
+        end
+        vectorize_variables!(V, submodel, state_or_prm[k], mapper[k], config = c)
     end
     return V
 end
 
-function devectorize_variables!(state_or_prm, model::MultiModel, V, type_or_map = :primary)
+function devectorize_variables!(state_or_prm, model::MultiModel, V, type_or_map = :primary; config = nothing)
     mapper = get_mapper_internal(model, type_or_map)
     for (k, submodel) in pairs(model.models)
-        devectorize_variables!(state_or_prm[k], submodel, V, mapper[k])
+        if isnothing(config)
+            c = nothing
+        else
+            c = config[k]
+        end
+        devectorize_variables!(state_or_prm[k], submodel, V, mapper[k], config = c)
     end
     return state_or_prm
 end
--- src/variables/vectorization.jl
+++ src/variables/vectorization.jl
@@ -1,37 +1,45 @@
 export vectorize_variables, vectorize_variables!, devectorize_variables!
 
 
-function vectorize_variables(model, state_or_prm, type_or_map = :primary)
+function vectorize_variables(model, state_or_prm, type_or_map = :primary; config = nothing)
     mapper = get_mapper_internal(model, type_or_map)
     n = sum(x -> x.n, values(mapper), init = 0)
     V = zeros(n)
-    return vectorize_variables!(V, model, state_or_prm, mapper)
+    return vectorize_variables!(V, model, state_or_prm, mapper, config = config)
 end
 
-function vectorize_variables!(V, model, state_or_prm, type_or_map = :primary)
+function vectorize_variables!(V, model, state_or_prm, type_or_map = :primary; config = nothing)
     mapper = get_mapper_internal(model, type_or_map)
     for (k, v) in mapper
-        (; n, offset) = v
-        state_val = state_or_prm[k]
-        @assert length(state_val) == n "Expected field $k to have length $n, was $(length(state_val))"
-        for i in 1:n
-            V[offset+i] = state_val[i]
-        end
+        vectorize_variable!(V, state_or_prm, k, v)
     end
     return V
 end
 
-function devectorize_variables!(state_or_prm, model, V, type_or_map = :primary)
+function vectorize_variable!(V, state, k, info)
+    (; n, offset) = info
+    state_val = state[k]
+    @assert length(state_val) == n "Expected field $k to have length $n, was $(length(state_val))"
+    for i in 1:n
+        V[offset+i] = state_val[i]
+    end
+end
+
+function devectorize_variables!(state_or_prm, model, V, type_or_map = :primary; config = nothing)
     mapper = get_mapper_internal(model, type_or_map)
     for (k, v) in mapper
-        state_val = state_or_prm[k]
-        (; n, offset) = v
-        @assert length(state_val) == n "Expected field $k to have length $n, was $(length(state_val))"
-        for i in 1:n
-            state_val[i] = V[offset+i]
-        end
+        devectorize_variable!(state_or_prm, V, k, v)
     end
     return state_or_prm
+end
+
+function devectorize_variable!(state, V, k, info)
+    (; n, offset) = info
+    state_val = state[k]
+    @assert length(state_val) == n "Expected field $k to have length $n, was $(length(state_val))"
+    for i in 1:n
+        state_val[i] = V[offset+i]
+    end
 end
 
 get_mapper_internal(model, type_or_map::Symbol) = first(variable_mapper(model, type_or_map))
