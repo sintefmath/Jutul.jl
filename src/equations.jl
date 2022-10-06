555f892fe6f9179272f9b7193a2cc6250cd9affc
Olav Møyner <olav.moyner@gmail.com>
2022-10-06 11:18:16 +0200

    Fix to active set for sparsity

--- src/equations.jl
+++ src/equations.jl
@@ -153,11 +153,12 @@ function create_equation_caches(model, equations_per_entity, number_of_entities,
                 unique!(S[i])
             end
         end
-        number_of_entities_source, T = epack
-        S, number_of_entities_source = remap_sparsity!(S, self_entity, e, model, number_of_entities_source)
+        _, T = epack
+        S, number_of_entities_source = remap_sparsity!(S, e, model)
         has_diagonal = number_of_entities == number_of_entities_total && is_self
         @assert number_of_entities_total > 0 && number_of_entities_source > 0 "nt=$number_of_entities_total ns=$number_of_entities_source for $T"
-        @timeit "cache alloc" caches[Symbol(e)] = GenericAutoDiffCache(T, equations_per_entity, e, S, number_of_entities_total, number_of_entities_source, has_diagonal = has_diagonal)
+        @timeit "cache alloc" cache = GenericAutoDiffCache(T, equations_per_entity, e, S, number_of_entities_total, number_of_entities_source, has_diagonal = has_diagonal)
+        caches[Symbol(e)] = cache
     end
     if !self_entity_found
         caches[:numeric] = zeros(equations_per_entity, number_of_entities)
@@ -165,31 +166,25 @@ function create_equation_caches(model, equations_per_entity, number_of_entities,
     return convert_to_immutable_storage(caches)
 end
 
-function remap_sparsity!(S, eq_entity, var_entity, eq_model, ns)
+function remap_sparsity!(S, var_entity, eq_model)
     # Filter away inactive entities in sparsity
     d_e = eq_model.domain
     map_e = global_map(d_e)
-    if map_e isa TrivialGlobalMap
-        S_filtered = S
-    else
+    ns = count_active_entities(d_e, var_entity)
+    if !(map_e isa TrivialGlobalMap)
         map_e = global_map(d_e)
-        active_eqs = active_entities(d_e, map_e, eq_entity, for_variables = false)
-        ns = count_active_entities(d_e, var_entity)
+        active_vars = active_entities(d_e, map_e, var_entity, for_variables = false)
         n = length(S)
-        keep = BitVector(undef, n)
         for i in 1:n
-            in_set = i in active_eqs
-            keep[i] = in_set
+            # Alter the set - renumber and remove entries that are not a part of local set
             s = S[i]
-            if in_set
-                for j in eachindex(s)
-                    s[j] = index_map(s[j], map_e, VariableSet(), EquationSet(), var_entity)
-                end
+            filter!(x -> x in active_vars, s)
+            for j in eachindex(s)
+                s[j] = index_map(s[j], map_e, VariableSet(), EquationSet(), var_entity)
             end
         end
-        S_filtered = S[keep]
     end
-    return (S_filtered, ns)
+    return (S, ns)
 end
 
 """
