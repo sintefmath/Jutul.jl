3f700cde4fc3b8f9e501b74d58f8b63dda282467
Olav Møyner <olav.moyner@sintef.no>
2022-10-31 15:01:05 +0100

    Improve opt config missing field

--- src/multimodel/gradients.jl
+++ src/multimodel/gradients.jl
@@ -115,7 +115,7 @@ function optimization_config(model::MultiModel, param, active = nothing; kwarg..
     out = Dict{Symbol, Any}()
     for k in submodel_symbols(model)
         m = model[k]
-        if isnothing(active)
+        if isnothing(active) || !haskey(active, k)
             v = optimization_config(m, param[k]; kwarg...)
         else
             v = optimization_config(m, param[k], active[k]; kwarg...)
