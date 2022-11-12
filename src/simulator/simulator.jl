715ec64511e9e8fe935316e221a5bb3b499d2c7b
Olav Møyner <olav.moyner@gmail.com>
2022-11-12 12:51:29 +0100

    Fix true/false bug in last commit

--- src/simulator/simulator.jl
+++ src/simulator/simulator.jl
@@ -374,7 +374,7 @@ function initial_setup!(sim, config, timesteps; restart = nothing, parameters = 
         reset_variables!(sim, state0)
     end
     if recompute_state0_secondary
-        update_secondary_variables!(sim.storage, sim.model, false)
+        update_secondary_variables!(sim.storage, sim.model, true)
     end
     return (states, reports, first_step, dt)
 end
