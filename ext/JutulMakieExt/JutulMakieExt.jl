74462cc1fbb35c479d7bf8b96e7e139965fa9e5d
Olav Møyner <olav.moyner@gmail.com>
2023-03-22 09:46:09 +0100

    Add error message when plotting is not possible

--- ext/JutulMakieExt/JutulMakieExt.jl
+++ ext/JutulMakieExt/JutulMakieExt.jl
@@ -1,6 +1,9 @@
 module JutulMakieExt
 
 using Jutul, Makie
+    function Jutul.check_plotting_availability_impl()
+        return true
+    end
 
     include("mesh_plots.jl")
 
--- src/makie_ext.jl
+++ src/makie_ext.jl
@@ -1,7 +1,26 @@
 function plot_interactive(arg...; kwarg...)
+    check_plotting_availability()
     plot_interactive_impl(arg...; kwarg...)
 end
 
 function plot_interactive_impl
+
+end
+
+function check_plotting_availability()
+    ok = true
+    try
+        ok = check_plotting_availability_impl()
+    catch e
+        if e isa MethodError
+            error("Plotting is not available. You need to have a Makie backend available. For 3D plots, GLMakie is recommended. To fix: using Pkg; Pkg.add(\"GLMakie\")")
+        else
+            rethrow(e)
+        end
+    end
+    return ok
+end
+
+function check_plotting_availability_impl
 
 end
