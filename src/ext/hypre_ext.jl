7e093d47723b3241abdea13ebb8bdc9d38824f5c
Olav Møyner <olav.moyner@gmail.com>
2023-04-18 09:29:06 +0200

    Set defaults for BoomerAMG

--- src/ext/hypre_ext.jl
+++ src/ext/hypre_ext.jl
@@ -11,10 +11,26 @@ end
 
 export BoomerAMGPreconditioner
 
-function BoomerAMGPreconditioner(; kwarg...)
+function BoomerAMGPreconditioner(;
+    CoarsenType = 10,      # PMIS
+    StrongThreshold = 0.7, # For 3D
+    AggNumLevels = 2,      # Aggressive coarsening for first levels
+    AggTruncFactor = 0.3,  # Remove weak connections
+    InterpType = 6,        # ext+i
+    kwarg...
+    )
+    # Default settings inspired by
+    # https://mooseframework.inl.gov/releases/moose/2021-05-18/application_development/hypre.html
     prec = missing
     try
-        prec = setup_hypre_precond(:boomeramg; kwarg...)
+        prec = setup_hypre_precond(:boomeramg;
+            CoarsenType = CoarsenType,
+            StrongThreshold = StrongThreshold,
+            AggNumLevels = AggNumLevels,
+            AggTruncFactor = AggTruncFactor,
+            InterpType = InterpType,
+            kwarg...
+            )
     catch e
         @error "Unable to initialize HYPRE preconditioner. Is HYPRE.jl loaded and Julia at least 1.9?"
         rethrow(e)
