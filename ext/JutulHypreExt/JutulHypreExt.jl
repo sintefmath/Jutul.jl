a60078dff51506a203ed8ae19d566a763d82bb60
Olav Møyner <olav.moyner@gmail.com>
2023-04-18 13:17:25 +0200

    Improve allocations for HYPRE

--- ext/JutulHypreExt/JutulHypreExt.jl
+++ ext/JutulHypreExt/JutulHypreExt.jl
@@ -40,7 +40,8 @@ module JutulHypreExt
             J_h = HYPRE.HYPREMatrix(J)
             D[:J] = J
             D[:n] = n
-            D[:indices] = collect(1:n)
+            (; ilower, iupper) = r_h
+            D[:vector_indices] = HYPRE.HYPRE_BigInt.(ilower:iupper)
         end
         D[:converted] = (J_h, r_h, x_h)
         HYPRE.@check HYPRE.HYPRE_BoomerAMGSetup(preconditioner.prec, J_h, r_h, x_h)
@@ -52,27 +53,56 @@ module JutulHypreExt
     end
 
     function Jutul.apply!(x, p::BoomerAMGPreconditioner, y, arg...)
-        ix = p.data[:indices]
+        ix = p.data[:vector_indices]
         J_h, y_h, x_h = p.data[:converted]
-        asm = HYPRE.start_assemble!(x_h)
-        HYPRE.finish_assemble!(asm)
-        assembler = HYPRE.start_assemble!(y_h)
-        HYPRE.assemble!(assembler, ix, y)
-        HYPRE.finish_assemble!(assembler)
-        # HYPRE.@check HYPRE.HYPRE_BoomerAMGSetup(p.prec, J_h, y_h, x_h)
-        HYPRE.@check HYPRE.HYPRE_BoomerAMGSolve(p.prec, J_h, y_h, x_h)
-        copy!(x, x_h)
+        inner_apply!(x, y, p.prec, x_h, J_h, y_h, ix)
+    end
+
+    function inner_apply!(x, y, prec, x_h, J_h, y_h, ix)
+        # Safe and allocating version that uses HYPRE.jl API
+        # asm = HYPRE.start_assemble!(x_h)
+        # HYPRE.finish_assemble!(asm)
+        # assembler = HYPRE.start_assemble!(y_h)
+        # HYPRE.assemble!(assembler, ix, y)
+        # HYPRE.finish_assemble!(assembler)
+        # HYPRE.@check HYPRE.HYPRE_BoomerAMGSolve(prec, J_h, y_h, x_h)
+        # copy!(x, x_h)
+
+        # Fast and less allocating version that uses low level HYPRE calls
+        local_copy!(y_h, y, ix)
+        @. x = 0.0
+        local_copy!(x_h, x, ix)
+        HYPRE.@check HYPRE.HYPRE_BoomerAMGSolve(prec, J_h, y_h, x_h)
+        local_copy!(x, x_h, ix)
+    end
+
+    function hypre_check(dst::HYPRE.HYPREVector, src::Vector, ix)
+        @assert dst.parvector != C_NULL
+        nvalues = length(src)
+        @assert length(ix) == nvalues
+        return nvalues
+    end
+
+    function local_copy!(dst::Vector{HYPRE.HYPRE_Complex}, src::HYPRE.HYPREVector, ix::Vector{HYPRE.HYPRE_BigInt})
+        nvalues = hypre_check(src, dst, ix)
+        HYPRE.@check HYPRE.HYPRE_IJVectorGetValues(src, nvalues, ix, dst)
+    end
+
+    function local_copy!(dst::HYPRE.HYPREVector, src::Vector{HYPRE.HYPRE_Complex}, ix::Vector{HYPRE.HYPRE_BigInt})
+        nvalues = hypre_check(dst, src, ix)
+        HYPRE.@check HYPRE.HYPRE_IJVectorSetValues(dst, nvalues, ix, src)
+        HYPRE.Internals.assemble_vector(dst)
     end
 
     function reassemble_internal_boomeramg!(I_buf, J_buf, V_buffers, nzval, J, J_h, rows)
         n = size(J, 1)
         @assert length(I_buf) == 1
         assembler = HYPRE.start_assemble!(J_h)
-        for row in 1:n
+        @inbounds for row in 1:n
             row_ix = nzrange(J, row)
             k = length(row_ix)
             ROWS = I_buf
-            @inbounds ROWS[1] = row
+            ROWS[1] = row
             COLS = J_buf
             V_buf = V_buffers[k]
             resize!(COLS, k)
