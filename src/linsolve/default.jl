13cdab551d7e5d96ee875fcd5c1882586a1edc8d
Olav Møyner <olav.moyner@gmail.com>
2023-06-06 21:35:24 +0200

    Split linear solve timing into two parts

--- src/linsolve/default.jl
+++ src/linsolve/default.jl
@@ -290,15 +290,9 @@ end
 
 block_size(lsys::LSystem) = 1
 
-linear_solve_return(ok = true, iterations = 1, stats = nothing) = (ok = ok, iterations = iterations, stats = deepcopy(stats))
-
-# function linear_solve!(sys::LSystem, linsolve, model, storage = nothing, dt = nothing, recorder = nothing, executor = default_executor(); kwarg...)
-#     linear_solve!(sys, linsolve; kwarg...)
-# end
-
-# function linear_solve!(sys::LSystem, linsolve::Nothing; kwarg...)
-#     linear_solve!(sys; kwarg...)
-# end
+function linear_solve_return(ok = true, iterations = 1, stats = nothing; prepare = 0.0)
+    (ok = ok, iterations = iterations, stats = (stats = deepcopy(stats), prepare = prepare))
+end
 
 function linear_solve!(sys, ::Nothing, arg...; dx = sys.dx, r = sys.r, atol = nothing, rtol = nothing, executor = default_executor())
     limit = 100_000
