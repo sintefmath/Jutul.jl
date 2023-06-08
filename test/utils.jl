f1626a461551eabfac5e09eff807f53b1f3155f9
Olav Møyner <olav.moyner@gmail.com>
2023-06-08 15:36:06 +0200

    Update test to account for new fields

--- test/utils.jl
+++ test/utils.jl
@@ -111,7 +111,25 @@ end
     d[:data_3d] = d3d
     @test d[:data_3d] == d3d
     @test_throws "AssertionError: Number of columns for Matrix scalar defined on Cells() should be 6, was 2" d[:scalar] = rand(93, 2)
-    @test tuple(keys(d)...) == (:neighbors, :areas, :normals, :face_centroids, :cell_centroids, :volumes, :cell_vector, :face_vector, :scalar, :data_2d, :data_3d)
+    @test tuple(keys(d)...) == (
+        :neighbors,
+        :areas,
+        :normals,
+        :face_centroids,
+        :cell_centroids,
+        :volumes,
+        :half_face_cells,
+        :half_face_faces,
+        :boundary_areas,
+        :boundary_centroids,
+        :boundary_normals,
+        :boundary_neighbors,
+        :cell_vector,
+        :face_vector,
+        :scalar,
+        :data_2d,
+        :data_3d
+        )
 end
 
 @testset "get_1d_interpolator" begin
