986aeeb0e91b56fe84bca14fbf5315e75e03672d
Olav Møyner <olav.moyner@gmail.com>
2023-12-02 12:22:10 +0100

    Avoid some intermediate allocations in plotting

--- src/meshes/unstructured/plotting.jl
+++ src/meshes/unstructured/plotting.jl
@@ -1,28 +1,39 @@
-function Jutul.triangulate_mesh(m::UnstructuredMesh{3}; is_depth = true, outer = false)
-    pts = Vector{Matrix{Float64}}()
-    tri = Vector{Matrix{Int64}}()
+function Jutul.triangulate_mesh(m::UnstructuredMesh{3}; is_depth = true, outer = false, flatten = true)
+    N = 3
+    pts = Vector{SVector{N, Float64}}()
+    tri = Vector{SVector{N, Int64}}()
     cell_index = Vector{Int64}()
     face_index = Vector{Int64}()
     node_pts = m.node_points
     T = eltype(node_pts)
-    function cyclical_tesselation(n, offset::Int)
+    function cyclical_tesselation(n::Int, i::Int, offset::Int)
         # Create a triangulation of face, assuming convexity
         # Each tri is two successive points on boundary connected to centroid
-        out = Matrix{Int}(undef, n, 3)
-        for i in 1:n
-            # First column i + 1
-            out[i, 1] = i + 1 + offset
-            # Second column: Always the center point
-            out[i, 2] = 1 + offset
-            # Third column: Wrap-around to first column
-            if i == 1
-                out[i, 3] = n + 1 + offset
-            else
-                out[i, 3] = i + offset
-            end
+        # First column i + 1
+        # Second column: Always the center point
+        # Third column: Wrap-around to first column
+        if i == 1
+            t = n + offset + 1
+        else
+            t = i + offset
         end
-        return out
+        return @SVector [i + 1 + offset, 1 + offset, t]
     end
+
+    function local_point(center, i, nodes, node_pts)
+        if i == 0
+            pt = center
+        else
+            pt = node_pts[nodes[i]]
+        end
+        pt::SVector{N, Float64}
+        if is_depth
+            u = SVector{N, Float64}(1.0, 1.0, -1.0)
+            pt = pt .* u
+        end
+        return pt::SVector{N, Float64}
+    end
+
     offset = 0
 
     function add_points!(e, faces)
@@ -34,36 +45,18 @@ function Jutul.triangulate_mesh(m::UnstructuredMesh{3}; is_depth = true, outer =
                 C += node_pts[node]
             end
             C /= n
-
-            local_pts = zeros(n+1, 3)
-            for i = 1:3
-                xyz = C[i]
-                if i == 3 && is_depth
-                    xyz = -xyz
-                end
-                local_pts[1, i] = xyz
-            end
-            for (i, n) in enumerate(nodes)
-                for d in 1:3
-                    xyz = node_pts[n][d]
-                    if d == 3 && is_depth
-                        xyz = -xyz
-                    end
-                    local_pts[i+1, d] = xyz
-                end
-            end
             new_vert_count = n + 1
 
             for cell in faces.neighbors[f]
                 if cell > 0
-                    for _ in 1:new_vert_count
+                    for i in 1:new_vert_count
                         push!(cell_index, cell)
                         push!(face_index, f)
+                        push!(pts, local_point(C, i-1, nodes, node_pts))
                     end
-                    tri_cell = cyclical_tesselation(n, offset)::Matrix{Int}
-                    push!(pts, local_pts)
-                    push!(tri, tri_cell)
-
+                    for i in 1:n
+                        push!(tri, cyclical_tesselation(n, i, offset))
+                    end
                     offset = offset + new_vert_count
                 end
             end
@@ -75,8 +68,10 @@ function Jutul.triangulate_mesh(m::UnstructuredMesh{3}; is_depth = true, outer =
     end
     add_points!(BoundaryFaces(), m.boundary_faces)
 
-    pts = plot_flatten_helper(pts)
-    tri = plot_flatten_helper(tri)
+    if flatten
+        pts = plot_flatten_helper(pts)
+        tri = plot_flatten_helper(tri)
+    end
 
     mapper = (
                 Cells = (cell_data) -> cell_data[cell_index],
@@ -84,6 +79,19 @@ function Jutul.triangulate_mesh(m::UnstructuredMesh{3}; is_depth = true, outer =
                 indices = (Cells = cell_index, Faces = face_index)
               )
     return (points = pts, triangulation = tri, mapper = mapper)
+end
+
+function plot_flatten_helper(data::Vector{Tv}) where Tv<:SVector
+    n = length(data)
+    m = length(Tv)
+    T = eltype(eltype(Tv))
+
+    out = zeros(T, n, m)
+    offset = 0
+    for i in 1:n
+        @. out[i, :] = data[i]
+    end
+    return out
 end
 
 function plot_flatten_helper(data)
