759dcfc4ab8e4d1d9748472aefca239d40cec315
Olav Møyner <olav.moyner@gmail.com>
2024-01-11 13:24:49 +0100

    Fix const table load order

--- src/units/time.jl
+++ src/units/time.jl
@@ -22,3 +22,15 @@ end
 function si_unit(::Val{:psi})
     return si_unit(:lbf)/(si_unit(:inch)^2)
 end
+
+const TIME_UNITS_FOR_PRINTING = (
+    (si_unit(:year), :year),
+    (7*si_unit(:day), :week),
+    (si_unit(:day), :day),
+    (si_unit(:hour), :hour),
+    (si_unit(:minute), :minute),
+    (si_unit(:second), :second),
+    (si_unit(:milli)*si_unit(:second), :millisecond),
+    (si_unit(:micro)*si_unit(:second), :microsecond),
+    (si_unit(:nano)*si_unit(:second), :nanosecond),
+)
