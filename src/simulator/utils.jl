3167dc119563275c28439ce76945df69455d9370
Olav Møyner <olav.moyner@gmail.com>
2024-01-11 13:24:49 +0100

    Fix const table load order

--- src/simulator/utils.jl
+++ src/simulator/utils.jl
@@ -1,15 +1,3 @@
-const TIME_UNITS_FOR_PRINTING = (
-    (si_unit(:year), :year),
-    (7*si_unit(:day), :week),
-    (si_unit(:day), :day),
-    (si_unit(:hour), :hour),
-    (si_unit(:minute), :minute),
-    (si_unit(:second), :second),
-    (si_unit(:milli)*si_unit(:second), :millisecond),
-    (si_unit(:micro)*si_unit(:second), :microsecond),
-    (si_unit(:nano)*si_unit(:second), :nanosecond),
-)
-
 """
     get_tstr(dT, lim = 3)
 
