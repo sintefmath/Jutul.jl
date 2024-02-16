127f18d6dd727a098401cf93491e00458ab6e9fb
Olav Møyner <olav.moyner@gmail.com>
2024-02-16 11:00:58 +0100

    Improved composite variable setting support

--- src/composite/variables.jl
+++ src/composite/variables.jl
@@ -16,3 +16,31 @@ function update_primary_variable!(state, p::Pair{Symbol, V}, state_symbol, model
     m = composite_submodel(model, label)
     update_primary_variable!(state, var, state_symbol, m, dx, w)
 end
+
+function set_variable_internal!(vars, model::CompositeModel; kwarg...)
+    for (k, v) in kwarg
+        if v isa Pair
+            # "Right" format for a composite model.
+            delete_variable!(model, k)
+            vars[k] = v
+        else
+            if haskey(vars, k)
+                vars[k]::Pair
+                oldkey, = vars[k]
+            else
+                oldkey = missing
+            end
+            if ismissing(oldkey)
+                mkeys = keys(model.system.systems)
+                if length(mkeys) == 1
+                    oldkey = only(mkeys)
+                else
+                    throw(ArgumentError("$k is not present in model.\nFor a composite model, this must be specified as a Pair with key as one of $mkeys.\nExample: $k = Pair(:$(first(mkeys)), var)"))
+                end
+            end
+            v::JutulVariables
+            delete_variable!(model, k)
+            vars[k] = Pair(oldkey, v)
+        end
+    end
+end
