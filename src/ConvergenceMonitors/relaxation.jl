c165617d31fbbaa4336a70b05c37bf7d1bc27134
Øystein Klemetsdal <oystein.klemetsdal@gmail.com>
2025-04-03 22:51:09 +0200

    Document relaxation

--- src/ConvergenceMonitors/relaxation.jl
+++ src/ConvergenceMonitors/relaxation.jl
@@ -5,6 +5,14 @@ struct ConvergenceMonitorRelaxation <: Jutul.NonLinearRelaxation
     dw_increase::Float64
 end
 
+"""
+    ConvergenceMonitorRelaxation(; w_min = 0.1, dw = 0.2, dw_increase = nothing, dw_decrease = nothing, w_max = 1.0)
+
+Relaxation strategy based on convergence monitoring. Requires that the
+ simulation has been configured with a `ConvergenceMonitorCuttingCriterion` so
+that the convergence status is available in the reports. See corresponding
+iplementation of `select_nonlinear_relaxation_model` below for details.
+"""
 function ConvergenceMonitorRelaxation(; w_min = 0.1, dw = 0.2, dw_increase = nothing, dw_decrease = nothing, w_max = 1.0)
     if isnothing(dw_increase)
         dw_increase = dw/2
@@ -15,6 +23,13 @@ function ConvergenceMonitorRelaxation(; w_min = 0.1, dw = 0.2, dw_increase = not
     return ConvergenceMonitorRelaxation(w_min, w_max, dw_decrease, dw_increase)
 end
 
+"""
+    set_convergence_monitor_relaxation!(config; max_nonlinear_iterations = 50, convergence_monitor_args = NamedTuple(), relaxation_args...)
+
+Utility for setting `ConvergenceMonitorRelaxation` to the simulator config. This
+function also sets the a `ConvergenceMonitorCuttingCriterion` to the config --
+see `set_convergence_monitor_cutting_criterion!`.
+"""
 function set_convergence_monitor_relaxation!(config; 
     max_nonlinear_iterations = 50,
     convergence_monitor_args = NamedTuple(),
@@ -30,7 +45,17 @@ function set_convergence_monitor_relaxation!(config;
 
 end
 
+"""
+    Jutul.select_nonlinear_relaxation_model(model, rel_type::ConvergenceMonitorRelaxation, reports, ω)
+
+Relaxation strategy based on convergence monitoring. The relaxation factor is
+decreased if the convergence status is "bad", and increased if the status is
+"good", determined by contraction factors computed from distance to convergence
+(in a user-defined metric) between consecutive iterates. These are computed
+during the cutting criterion check and stored in the reports.
+"""
 function Jutul.select_nonlinear_relaxation_model(model, rel_type::ConvergenceMonitorRelaxation, reports, ω)
+
     if length(reports) > 1
         (; dw_decrease, dw_increase, w_max, w_min) = rel_type
         
@@ -45,8 +70,8 @@ function Jutul.select_nonlinear_relaxation_model(model, rel_type::ConvergenceMon
         end
         ω = clamp(ω, w_min, w_max)
 
-        println("Relaxation: ", ω)
-
     end
+
     return ω
+
 end
