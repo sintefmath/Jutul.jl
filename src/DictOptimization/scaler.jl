ab70816976edf1ff349080a0cf847e8fb9fecfc1
Olav Møyner <olav.moyner@gmail.com>
2025-06-19 21:03:41 +0200

    Add more scaling code

--- src/DictOptimization/scaler.jl
+++ src/DictOptimization/scaler.jl
@@ -1,6 +1,10 @@
 function apply_scaler(x::Real, s::Symbol)
     if s == :log
-        x = log(x + 1e-20)
+        base = 1e5
+        x = log((base-1)*x + 1)/log(base)
+    elseif s == :exp
+        base = 1e5
+        x = (base^x - 1)/(base - 1)
     elseif s == :standard_log
         x = log(x)
     elseif s == :log10
@@ -21,11 +25,17 @@ end
 
 function undo_scaler(x::Real, s::Symbol)
     if s == :log
-        x = exp(x) - 1e-20
+        base = 1e5
+        x = (base^x - 1)/(base - 1)
+    elseif s == :exp
+        base = 1e5
+        x = log((base-1)*x + 1)/log(base)
     elseif s == :standard_log
         x = exp(x)
     elseif s == :log10
         x = 10^x
+    elseif s == :reciprocal
+        x = 1.0/x - 1e-20
     else
         error("Unknown scaler $s")
     end
