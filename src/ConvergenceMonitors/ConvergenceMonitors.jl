4255d4e90d11f85f4e77ca354cd8269eb2428cd8
Øystein Klemetsdal <oystein.klemetsdal@gmail.com>
2025-08-19 15:28:54 +0200

    Start adding support for per-equation convergence monitoring

--- src/ConvergenceMonitors/ConvergenceMonitors.jl
+++ src/ConvergenceMonitors/ConvergenceMonitors.jl
@@ -3,6 +3,7 @@ module ConvergenceMonitors
     using Jutul
 
     export ConvergenceMonitorCuttingCriterion
+    export compute_distance
     export set_convergence_monitor_cutting_criterion!
     export set_convergence_monitor_relaxation!
 
