60f87e4e99890b1cf2bfdab87b561000ab374050
Andreas Brostrøm <andreas.brostrom@sintef.no>
2025-08-29 16:09:24 +0200

    Change the progress recorder API

--- src/simulator/recorder.jl
+++ src/simulator/recorder.jl
@@ -4,38 +4,57 @@ step(r) = r.recorder.step
 substep(r) = r.subrecorder.step
 
 # Progress recorder stuff
-function nextstep_global!(r::ProgressRecorder, dT, prev_success = !isnan(r.recorder.dt))
-    g = r.recorder
-    l = r.subrecorder
-    g.iteration = l.iterations
-    # A bit dicey, just need to get this working
-    g.failed += l.failed
-    nextstep!(g, dT, prev_success)
-    reset!(r.subrecorder)
-end
-
-function nextstep_local!(r::ProgressRecorder, dT, prev_success = !isnan(r.local_recorder.dt))
-    nextstep!(r.subrecorder, dT, prev_success)
-end
-
-function next_iteration!(rec, report)
-    if haskey(report, :update_time)
-        rec.subrecorder.iteration += 1
+function recorder_start_step!(rec::ProgressRecorder, dt, level::Symbol)
+    level == :local || level == :global || error("level must be :local or :global")
+    if level == :local
+        rec.subrecorder.dt = dt
+    else # global
+        rec.recorder.dt = dt
     end
 end
 
-function nextstep!(l::SolveRecorder, dT, success)
-    # Update time
-    if success
-        l.step += 1
-        l.time += l.dt
-    else
-        l.failed += l.iteration
+function recorder_log_step!(rec::ProgressRecorder, success, level::Symbol)
+    level == :local || level == :global || error("level must be :local or :global")
+    l = rec.subrecorder
+    g = rec.recorder
+
+    function update!(solver_rec, success)
+        if success
+            solver_rec.step += 1
+            solver_rec.time += solver_rec.dt
+        else
+            solver_rec.failed += solver_rec.iteration
+        end
+        solver_rec.iterations += solver_rec.iteration
+        solver_rec.iteration = 0
     end
-    l.dt = dT
-    # Update iterations
-    l.iterations += l.iteration
-    l.iteration = 0
+
+    if level == :local
+        update!(l, success)
+    else # global
+        g.iteration = l.iterations
+        g.failed += l.failed
+        update!(g, success)
+        reset!(l)
+    end
+end
+
+function recorder_current_time(rec::ProgressRecorder, level::Symbol)
+    level == :local || level == :global || error("level must be :local or :global")
+    if level == :local
+        return rec.subrecorder.time
+    else # global
+        return rec.recorder.time + rec.subrecorder.time
+    end
+end
+
+function recorder_increment_iteration!(rec::ProgressRecorder, report, level::Symbol)
+    level == :local || level == :global || error("level must be :local or :global")
+    if level == :local
+        if haskey(report, :update_time)
+            rec.subrecorder.iteration += 1
+        end
+    end
 end
 
 function reset!(r::SolveRecorder, dt = NaN; step = 1, iterations = 0, iteration = 0, time = 0.0)
@@ -63,8 +82,3 @@ function reset!(target::ProgressRecorder, source::ProgressRecorder)
     reset!(target.recorder, source.recorder)
     reset!(target.subrecorder, source.recorder)
 end
-
-function current_time(r::ProgressRecorder)
-    return r.recorder.time + r.subrecorder.time
-end
-
