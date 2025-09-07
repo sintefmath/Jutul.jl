fa79ef8f7379c8c9841f8e28acfb74470d732464
Olav Møyner <olav.moyner@gmail.com>
2025-09-07 20:20:15 +0200

    Reorg

---
+++ src/ad/AdjointsDI/split.jl
@@ -0,0 +1,25 @@
+
+function map_X_to_Y(F, X, model, parameters_map, state0_map, cache)
+    has_state0 = !ismissing(state0_map)
+    case = F(X, missing)
+    N_prm = Jutul.vectorized_length(model, parameters_map)
+    if has_state0
+        N_s0 = Jutul.vectorized_length(model, state0_map)
+    else
+        N = N_prm
+    end
+    N = N_prm + N_s0
+    T = eltype(X)
+    if !haskey(cache, T)
+        cache[T] = zeros(T, N)
+    end
+    Y = cache[T]
+    resize!(Y, N)
+    Y_prm = view(Y, 1:N_prm)
+    vectorize_variables!(Y_prm, model, parameters_map, parameters_map)
+    if has_state0
+        Y_s0 = view(Y, (N_prm+1):(N_prm+N_s0))
+        vectorize_variables!(Y_s0, model, state0_map, state0_map)
+    end
+    return Y
+end
