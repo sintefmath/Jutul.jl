083deb11415a527bc69037ff4609f545b344356d
Øystein Klemetsdal <oystein.klemetsdal@gmail.com>
2025-09-14 21:49:26 +0200

    Clean up and restructure distance computations, including support for pooling

--- src/ConvergenceMonitors/distance_functions.jl
+++ src/ConvergenceMonitors/distance_functions.jl
@@ -1,3 +1,4 @@
+using LinearAlgebra
 """
     compute_distance(report; distance_function = r -> scaled_residual_norm(r), mapping = v -> maximum(v))
 
@@ -5,51 +6,56 @@ Compute distance from convergence using a user-defined distance function, and
 optionally apply a mapping to the distance. The function returns the distance
 and the names of equation residual norms used in the distance computation.
 """
-function compute_distance(report; 
-    distance_function = r -> scaled_residual_norm(r),
-    mapping = v -> [maximum(v)]
+function compute_distance(report;
+    distance_function = v -> max.(v, 1.0),
+    pools = nothing,
+    pooling_norm = Inf,
+    pooling_args = NamedTuple(),
     )
     
-    # Compute distance using distance function
-    distance, names = distance_function(report)
-    # Apply transformation
-    distance = mapping(distance)
-    if !(distance isa Vector)
-        distance = [distance]
+    # Compute scaled residual measures
+    measures = residual_measures(report)
+    m, names = flatten_dict(measures)
+    δ = distance_function(m)
+    if !isnothing(pools)
+        @assert length(δ) == length(m) "Pooling is only supported if number 
+        of distances equals number of residual measures"
+        δ, names = pool_distances(δ, names, pools, pooling_norm; pooling_args...)
+    end
+    δ = δ isa Number ? [δ] : δ
+
+    return δ, names
+
+end
+
+function pool_distances(d, names::Vector{String}, pools::Vector{Vector{Int64}}, p=Inf; check_coverage=true)
+
+    if check_coverage
+        pix = unique(vcat(pools...))
+        @assert all([k ∈ pix for k in 1:length(d)]) "
+        Pools do not cover all residual measures"
+    end
+    m_pooled = Vector{Float64}(undef, length(pools))
+    pools_str = Vector{Vector{String}}(undef, length(pools))
+    for (k, pool) in enumerate(pools)
+        mp = norm(max.(d[pool].-1, 0.0), p) .+ 1
+        m_pooled[k] = mp
+        pools_str[k] = names[pool]
     end
 
-    return distance, names
+    return m_pooled, pools_str
 
 end
 
-"""
-    scaled_residual_norm(report)
+function pool_distances(d, names::Vector{String}, pools::Union{Vector{String}, Vector{Regex}}, args...;
+    search_fn=contains, kwargs...)
 
-Compute distance to convergence as the residual norm of the equations scaled by
-their respective tolerances.
-"""
-function scaled_residual_norm(report)
+    pools_int = Vector{Vector{Int64}}(undef, length(pools))
+    for (k, pool) in enumerate(pools)
+        ix = findall(search_fn.(names, pool))
+        pools_int[k] = ix
+    end
 
-    residuals = get_multimodel_residuals(report)
-    values, names = flatten_dict(residuals)
-    distance = max.(values .- 1.0, 0.0)
-
-    return distance, names
-
-end
-
-"""
-    nonconverged_equations(report)
-
-Compute distance to convergence as non-converged equations, (1.0 for
-non-converged and 0.0 for converged). The final distance is typically taken as
-the sum of the output from this function.
-"""
-function nonconverged_equations(report)
-
-    values, names = scaled_residual_norm(report)
-    distance = (values .> 0.0) .+ 0.0
-
-    return distance, names
+    return pool_distances(d, names, pools_int, args...; kwargs...)
 
 end
