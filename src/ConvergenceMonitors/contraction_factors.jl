fa65fb803fb4e2e2b7ba10919ec63866a8334ff5
Øystein Klemetsdal <oystein.klemetsdal@gmail.com>
2025-09-14 21:50:19 +0200

    Update contraction factor computations and factor out key functionality

--- src/ConvergenceMonitors/contraction_factors.jl
+++ src/ConvergenceMonitors/contraction_factors.jl
@@ -7,23 +7,22 @@ iterates, the contraction factor is estimated using least-squares assuming the
 iterates follow a geometric series: rₙ = Θⁿr₀. The function also computes the
 target contraction factor under this assumption assuming N iterations left.
 """
-function compute_contraction_factor(d, N)
+function compute_contraction_factor(distances, N)
 
-    # Use distance from convergence + 1 to avoid division by small numbers
-    r = d .+ 1.0
     # Number of iterates used to estimate contraction factor
-    n = size(r,1)
+    δ = distances
+    n = size(δ,1)
     # Use least-squares regression on log-transformed contraction factors:
     # minₖ ∑(θ^{k-1} - rₖ/r₁)² ~ minₖ ∑[(k-1)log(θ) - log(rₖ/r₁)]²
     # ⇒ log(θ) ∑(k-1)² = ∑(k-1)log(rₖ/r₁)
-    num, den = zeros(size(r,2)), 0.0
+    num, den = zeros(size(δ,2)), 0.0
     for k = 1:n
-        num .+= (k-1).*log.(r[k,:]./r[1,:])
+        num .+= (k-1).*log.(δ[k,:]./δ[1,:])
         den += (k-1)^2
     end
-    θ = exp.(num./den)
+    θ = exp.(num./max(den,1))
     # Compute target contraction to converge in N iterations
-    θ_target = r[end,:].^(-1/N)
+    θ_target = δ[end,:].^(-1/N)
     
     return θ, θ_target
 
@@ -39,20 +38,17 @@ function returns true if the contraction factors are oscillating, and false
 otherwise. If less than three contraction factors are provided, the function
 returns false.
 """
-function oscillation(distance, tol=1e-6)
+function oscillation(distance_1, distace_2, distance_3, tol=1e-6)
 
-
-    # Early return if we only have two iterates
-    (size(distance,1) < 3) ? (return falses(size(distance,2))) : nothing
+    # # Early return if we only have two iterates
+    # (size(distance,1) < 3) ? (return falses(size(distance,2))) : nothing
     # Get last three distances to convergence
-    Δ1 = distance[end-2,:]
-    Δ2 = distance[end-1,:]
-    Δ3 = distance[end,:]
+    δ_1, δ_2, δ_3 = distance_1, distace_2, distance_3
     # Check if Δ1 > Δ2 < Δ3 or Δ1 < Δ2 > Δ3
     gt_tol = (x,y) -> (x .- y) .> tol
     lt_tol = (x,y) -> gt_tol(y,x)
-    osc_1 = lt_tol(Δ2, Δ1) .&& lt_tol(Δ2, Δ3)
-    osc_2 = gt_tol(Δ2, Δ1) .&& gt_tol(Δ2, Δ3)
+    osc_1 = lt_tol(δ_2, δ_1) .&& lt_tol(δ_2, δ_3)
+    osc_2 = gt_tol(δ_2, δ_1) .&& gt_tol(δ_2, δ_3)
     osc = osc_1 .|| osc_2
 
     return osc
@@ -67,8 +63,66 @@ function iterations_left(contraction_factor, dist)
     r = dist .+ 1
     N = ceil.(-log.(r)./log.(θ))
     N = max.(N, 0)
-    # ok = isfinite.(N)
-    # N[ok] .= Int64.(N[ok])
     return N
+
+end
+
+function classify_iterate(theta, theta_target, theta_slow, theta_fast, distance, oscillation=missing)
+
+    # Shorthand notation
+    θ, θ_t, θ_s, θ_f = theta, theta_target, theta_slow, theta_fast
+    osc = ismissing(oscillation) ? false : oscillation
+    # Check convergence for all distances
+    conv = distance .<= 1.0
+    # Check contraction magnitude
+    fast = (θ .<= max.(θ_t, θ_f))
+    ok = max.(θ_t, θ_f) .< θ .<= θ_s
+    div = θ .> θ_s
+    # Exclude "diverging" distances that have converged (e.g., θ = 1/1)
+    div = div .&& .!conv
+    # Classify fast contractions as ok if oscillating
+    ok = ok .|| (fast .&& osc)
+    # Classify converged distances as fast and require no oscillation
+    fast = (fast .|| conv) .&& .!osc
+    # Sanity check
+    @assert all(fast .+ ok .+ div .== 1)
+    # Set status
+    status = zeros(length(θ))
+    status[fast] .= 1
+    status[ok] .= 0
+    status[div] .= -1
+    # Return
+    return status
+
+end
+
+function update_violation_count!(num_violations, status; count_type=:per_measure)
+
+    if isnothing(num_violations)
+        if count_type == :per_measure
+            num_violations = zeros(length(status))
+        else
+            num_violations = [0]
+        end
+    end
+
+    fast = status .== 1
+    diverge = status .== -1
+    if count_type == :per_measure
+        num_violations[fast] .-= 1
+        num_violations[diverge] .+= 1
+    elseif count_type == :total
+        num_violations .+= sum(diverge) - sum(fast)
+    elseif count_type == :overall
+        if all(fast)
+            num_violations .-= 1
+        elseif any(diverge)
+            num_violations .+=1
+        end
+    end
+
+    num_violations = max.(num_violations, 0)
+
+    return num_violations
 
 end
