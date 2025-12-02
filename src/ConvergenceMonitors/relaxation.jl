a6dd43a6909b80a875e9fc30923185c7c973c9f8
Øystein Klemetsdal <oystein.klemetsdal@gmail.com>
2025-12-02 22:11:27 +0100

    Only increas relaxation when oscillating, and add warmup its before invoking

--- src/ConvergenceMonitors/relaxation.jl
+++ src/ConvergenceMonitors/relaxation.jl
@@ -3,6 +3,7 @@ struct ConvergenceMonitorRelaxation <: Jutul.NonLinearRelaxation
     w_max::Float64
     dw_decrease::Float64
     dw_increase::Float64
+    warmup_iterations::Int
 end
 
 """
@@ -13,14 +14,14 @@ Relaxation strategy based on convergence monitoring. Requires that the
 that the convergence status is available in the reports. See corresponding
 iplementation of `select_nonlinear_relaxation_model` below for details.
 """
-function ConvergenceMonitorRelaxation(; w_min = 0.1, dw = 0.2, dw_increase = nothing, dw_decrease = nothing, w_max = 1.0)
+function ConvergenceMonitorRelaxation(; w_min = 0.1, dw = 0.2, dw_increase = nothing, dw_decrease = nothing, w_max = 1.0, warmup_iterations = 5)
     if isnothing(dw_increase)
         dw_increase = dw/2
     end
     if isnothing(dw_decrease)
         dw_decrease = dw
     end
-    return ConvergenceMonitorRelaxation(w_min, w_max, dw_decrease, dw_increase)
+    return ConvergenceMonitorRelaxation(w_min, w_max, dw_decrease, dw_increase, warmup_iterations)
 end
 
 """
@@ -56,18 +57,29 @@ during the cutting criterion check and stored in the reports.
 """
 function Jutul.select_nonlinear_relaxation_model(model, rel_type::ConvergenceMonitorRelaxation, reports, ω)
 
-    if length(reports) > 1
+    if length(reports) > rel_type.warmup_iterations
         (; dw_decrease, dw_increase, w_max, w_min) = rel_type
         
         report = reports[end-1]
         @assert haskey(report, :convergence_monitor)
+        distance = report[:convergence_monitor][:distance]
         status = report[:convergence_monitor][:status]
         oscillating = report[:convergence_monitor][:oscillation]
+        oscillating = any(oscillating .&& (distance .> 1.0))
+        if any(status .== -1)
+            status = :bad
+        elseif all(status .== 1)
+            status = :good
+        else
+            status = :ok
+        end
 
-        if status == oscillating
+        if oscillating
             ω = ω - dw_decrease
+            println("Relaxation factor decreased to $ω due to oscillations.")
         elseif status ∈ [:good, :ok]
             ω = ω + dw_increase
+            println("Relaxation factor increased to $ω.")
         elseif status == :none
             # No change
         else
