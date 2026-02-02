8b38bbd3aedb41d011532123a61bc43311067f02
Olav Møyner <olav.moyner@gmail.com>
2026-02-02 19:31:08 +0100

    Update interface.jl

--- src/DictOptimization/interface.jl
+++ src/DictOptimization/interface.jl
@@ -109,7 +109,7 @@ function optimize(dopt::DictParameters, objective, setup_fn = dopt.setup_functio
         solution_history = false,
         print_parameters = false,
         allow_errors = false,
-        scale = optimizer == :lbfgsb_qp,
+        scale = optimizer != :lbfgsb_qp,
         gradient_scaling = true,
         kwarg...
     )
