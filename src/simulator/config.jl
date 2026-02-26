3ef25777c69366eb51b0ba1ad470be79d5f2a219
Olav Møyner <olav.moyner@gmail.com>
2026-02-26 10:40:27 +0100

    Option to preprocess output of states before IO

--- src/simulator/config.jl
+++ src/simulator/config.jl
@@ -51,6 +51,7 @@ Negative values disable output. The interpretation of this number is subject to 
     add_option!(cfg, :in_memory_reports, 10, "Limit for number of reports kept in memory if output_path is provided.", types = Int)
     add_option!(cfg, :report_level, 0, "Level of information stored in reports when written to disk.", types = Int)
     add_option!(cfg, :output_substates, false, "Store substates (between report steps) as field on each state.", types = Bool)
+    add_option!(cfg, :output_function, missing, "Function on the form (state, report) -> state that is run before output is written to disk or returned. This can be used to remove fields or add data. Note that it is easy to break the restart functionality if you modify the state in a non-compatible way. Use with care.")
 
     # Hooks
     add_option!(cfg, :post_ministep_hook, missing, "Hook to run after each ministep (successful or not) on format (done, report, sim, dt, forces, max_iter, cfg) -> (done, report)")
--- test/test_systems/io.jl
+++ test/test_systems/io.jl
@@ -5,7 +5,7 @@ function test_io()
     sys = ScalarTestSystem()
     D = ScalarTestDomain()
     model = SimulationModel(D, sys)
-    
+
     source = ScalarTestForce(1.0)
     forces = setup_forces(model, sources = source)
     state0 = setup_state(model, Dict(:XVar=>0.0))
@@ -32,7 +32,7 @@ function test_restart()
     sys = ScalarTestSystem()
     D = ScalarTestDomain()
     model = SimulationModel(D, sys)
-    
+
     source = ScalarTestForce(1.0)
     forces = setup_forces(model, sources = source)
     state0 = setup_state(model, Dict(:XVar=>0.0))
@@ -51,3 +51,73 @@ function test_restart()
 end
 
 test_restart()
+
+@testset "output_function" begin
+    sys = ScalarTestSystem()
+    D = ScalarTestDomain()
+    model = SimulationModel(D, sys)
+
+    source = ScalarTestForce(1.0)
+    forces = setup_forces(model, sources = source)
+    state0 = setup_state(model, Dict(:XVar=>0.0))
+    sim = Simulator(model, state0 = state0)
+    out = tempname()
+
+    function update_test_function(state, report)
+        state = deepcopy(state)
+        state[:XVar] = state[:XVar] .+ 1.0
+        if haskey(state, :substates)
+            for substate in state[:substates]
+                substate[:ExtraAddedField] = true
+            end
+        end
+        return state
+    end
+    states, reports = simulate(sim, [1.0, 2.0], forces = forces, output_states = true, output_path = out, info_level = -1)
+
+    states2, reports2 = simulate(sim, [1.0, 2.0], forces = forces,
+        restart = false,
+        output_path = out,
+        info_level = -1,
+        state0 = state0,
+        output_function = update_test_function
+    )
+
+    states2_mem, reports2_mem = simulate(sim, [1.0, 2.0], forces = forces,
+        restart = false,
+        info_level = -1,
+        state0 = state0,
+        output_function = update_test_function
+    )
+
+    @testset "Test output function" begin
+        for (s_mem, s_modif) in zip(states, states2)
+            @test s_mem[:XVar] .+ 1.0 == s_modif[:XVar]
+        end
+        for (s_mem, s_modif) in zip(states, states2_mem)
+            @test s_mem[:XVar] .+ 1.0 == s_modif[:XVar]
+        end
+    end
+
+    states3, reports3 = simulate(sim, [1.0, 2.0], forces = forces,
+        restart = false,
+        output_path = out,
+        info_level = -11,
+        max_timestep = 0.5,
+        output_substates = true,
+        state0 = state0,
+        output_function = update_test_function
+    )
+
+    @testset "Test output function" begin
+        for (s_mem, s_modif) in zip(states, states3)
+            @test s_mem[:XVar] .+ 1.0 == s_modif[:XVar]
+            @test haskey(s_modif, :substates)
+            for substate in s_modif[:substates]
+                @test haskey(substate, :ExtraAddedField)
+                @test substate[:ExtraAddedField] == true
+            end
+        end
+    end
+end
+
