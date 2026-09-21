5b015878a55f53e169b7daa088ff6cedc60826ef
Olav Møyner <olav.moyner@gmail.com>
2026-09-21 12:10:03 +0200

    Update linalg.jl

--- src/Benchmarking/linalg.jl
+++ src/Benchmarking/linalg.jl
@@ -1,14 +1,19 @@
-function benchmark_linear_operator(sim::Jutul.JutulSimulator;
+function benchmark_linear_operators(sim::Jutul.JutulSimulator, config = missing;
         n = 100,
+        n_update = ceil(Int, n / 20),
         verbose = false,
         warm = true,
         dt = si_unit(:day),
         forces = setup_forces(sim.model),
-        timer = TimerOutput()
+        timer = TimerOutput(),
+        executor = Jutul.default_executor()
     )
     model = Jutul.get_simulator_model(sim)
-    sys = sim.storage.LinearizedSystem
+    storage = sim.storage
+    sys = storage.LinearizedSystem
+    recorder = Jutul.progress_recorder(sim)
     dummy_timer = TimerOutput()
+    context = model.context
     x = missing
     function cycle(loc, x = missing)
         @timeit loc "linear_operator" op = Jutul.linear_operator(sys)
@@ -19,19 +24,48 @@ function benchmark_linear_operator(sim::Jutul.JutulSimulator;
         @timeit loc "mul!" mul!(x, op, r)
         return x
     end
-    update_state_dependents!(sim.storage, model, dt,
+    update_state_dependents!(storage, model, dt,
         forces,
         time = 0.0,
         update_secondary = true
     )
     # Warm up the linear operator before benchmarking
-    Jutul.update_linearized_system!(sim.storage, model)
-    Jutul.prepare_linear_solve!(sys)
+    if warm
+        Jutul.update_linearized_system!(storage, model)
+        Jutul.prepare_linear_solve!(sys)
+    end
 
     x = cycle(dummy_timer)
     @timeit timer "linear_operator" begin
-        for i in 1:n
+        for _ in 1:n
             x = cycle(timer, x)
+        end
+    end
+    if !ismissing(config)
+        lsolve = get(config, :linear_solver, missing)
+        if lsolve isa GenericKrylov
+            prec = lsolve.preconditioner
+            r = Jutul.vector_residual(sys)
+            update_prec() = Jutul.update_preconditioner!(prec, sys, context, model, storage, recorder, executor)
+            apply_prec() = apply!(x, prec, r)
+            if warm
+                update_prec()
+                apply_prec()
+            end
+            @timeit timer "preconditioner" begin
+                if verbose
+                    jutul_message("Benchmark", "Updating preconditioner $n_update times")
+                end
+                for _ in 1:n_update
+                    @timeit timer "update_preconditioner" update_prec()
+                end
+                if verbose
+                    jutul_message("Benchmark", "Applying preconditioner $n_update times")
+                end
+                for _ in 1:n_update
+                    @timeit timer "apply!" apply_prec()
+                end
+            end
         end
     end
     return timer
